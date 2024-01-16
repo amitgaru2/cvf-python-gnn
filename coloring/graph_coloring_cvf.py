3f3a5fc4eeb52c0769249dae8a2fa01bcb01f780
Amit Garu <agaru@blog1.cluster>
2024-01-16 10:11:29 -0700

    updates

--- coloring/graph_coloring_cvf.py
+++ coloring/graph_coloring_cvf.py
@@ -1,13 +1,22 @@
+import os
 import csv
 import copy
+import math
+import time
 
 import pandas as pd
 
 from itertools import combinations
 
-graph_name = "graph_powerlaw_cluster_graph_n9"
+start_time = time.time()
+
+graphs_dir = "graphs"
+results_dir = "results"
+
+graph_name = "graph_powerlaw_cluster_graph_n12"
+
 graph = {}
-with open(f"graphs/{graph_name}.txt", "r") as f:
+with open(os.path.join(graphs_dir, f"{graph_name}.txt"), "r") as f:
     line = f.readline()
     while line:
         node_edges = line.split()
@@ -16,22 +25,27 @@ with open(f"graphs/{graph_name}.txt", "r") as f:
         graph[node] = set(edges)
         line = f.readline()
 
+print(graph)
 
 nodes = list(graph.keys())
 node_positions = {v: i for i, v in enumerate(nodes)}
 
-degree_of_nodes = {n: len(graph[n]) for n in nodes}
-# max_len = max(len(graph[n]) for n in nodes)
-# degree_of_nodes = {n: max_len for n in nodes}
+max_degree = max(len(graph[n]) for n in nodes)
+degree_of_nodes = {n: max_degree for n in nodes}
 
-print("Degree of all nodes (starting from 0):", degree_of_nodes)
+# degree_of_nodes = {n: len(graph[n]) for n in nodes}
 
-configurations = {tuple([0 for i in range(len(nodes))])}
+print("Degree of all nodes (starting from 0):")
+print(degree_of_nodes) # start from 0
+
+configurations = {
+    tuple([0 for i in range(len(nodes))])
+}
 # perturb each state at a time for all states in configurations and accumulate the same in the configurations for next state to perturb
 for n in nodes:
     node_pos = node_positions[n]
     config_copy = copy.deepcopy(configurations)
-    for i in range(1, degree_of_nodes[n] + 1):
+    for i in range(1, degree_of_nodes[n]+1):
         for cc in config_copy:
             cc = list(cc)
             cc[node_pos] = i
@@ -42,137 +56,217 @@ print("No. of Configurations:", len(configurations))
 invariants = set()
 for state in configurations:
     all_paths = combinations(range(len(state)), 2)
-    for path in all_paths:
-        source = nodes[path[0]]
-        dest = nodes[path[1]]
-        source_color = state[path[0]]
-        dest_color = state[path[1]]
-        if dest in graph[source] and source_color == dest_color:
+    for src, dest in all_paths:
+        src_node, dest_node = nodes[src], nodes[dest]
+        src_color, dest_color = state[src], state[dest]
+        if dest_node in graph[src_node] and src_color == dest_color:
             # found same color node between neighbors
             break
     else:
         invariants.add(state)
 
-# print("Invariants and Count of Invariants:")
+print("Invariants and Count of Invariants:")
+print(len(invariants))
 
 program_transitions_rank = {}
 for inv in invariants:
     program_transitions_rank[inv] = {"L": 0, "C": 1, "A": 0, "Ar": 0, "M": 0}
 
-# print("Program transitions rank for invariants:")
 
-
-def find_min_color(colors):
-    for i in range(len(colors) + 1):
+def find_min_possible_color(colors):
+    for i in range(len(colors)+1):
         if i not in colors:
             return i
 
 
-def is_program_transition(perturb_pos, dest_state):
+def is_different_color(color, other_colors):
+    """
+    return True if "color" is different from all "other_colors"
+    """
+    for c in other_colors:
+        if color == c:
+            return False
+    return True
+
+
+
+def is_program_transition(perturb_pos, start_state, dest_state):
+    if start_state in invariants and dest_state in invariants:
+        return False
+
     node = nodes[perturb_pos]
     neighbor_pos = [node_positions[n] for n in graph[node]]
     neighbor_colors = set(dest_state[i] for i in neighbor_pos)
-    min_color = find_min_color(neighbor_colors)
+    min_color = find_min_possible_color(neighbor_colors)
     return dest_state[perturb_pos] == min_color
 
 
-def get_program_transitions_cvfs(start_state):
+def get_program_transitions(start_state):
     program_transitions = set()
-    cvfs = set()
     for position, val in enumerate(start_state):
-        possible_node_colors = set(range(degree_of_nodes[nodes[position]] + 1))
+        # check if node already has different color among the neighbors => If yes => no need to perturb that node's value
+        node = nodes[position]
+        neighbor_pos = [node_positions[n] for n in graph[node]]
+        neighbor_colors = set(start_state[i] for i in neighbor_pos)
+        if is_different_color(val, neighbor_colors):
+            continue
+        
+        # if the current node's color is not different among the neighbors => search for the program transitions possible
+        possible_node_colors = set(range(degree_of_nodes[nodes[position]]+1))
         for perturb_val in possible_node_colors:
             perturb_state = list(start_state)
             perturb_state[position] = perturb_val
             perturb_state = tuple(perturb_state)
             if perturb_state != start_state:
-                if is_program_transition(position, perturb_state):
+                if is_program_transition(position, start_state, perturb_state):
                     program_transitions.add(perturb_state)
-                else:
-                    cvfs.add(perturb_state)
-    return {"program_transitions": program_transitions, "cvfs": cvfs}
+    return {"program_transitions": program_transitions}
 
+
+def get_cvfs(start_state):
+    cvfs_in = set()
+    cvfs_out = set()
+    for position, _ in enumerate(start_state):
+        possible_node_colors = set(range(degree_of_nodes[nodes[position]]+1))
+        for perturb_val in possible_node_colors:
+            perturb_state = list(start_state)
+            perturb_state[position] = perturb_val
+            perturb_state = tuple(perturb_state)
+            if perturb_state != start_state:
+                if start_state in invariants:
+                    cvfs_in.add(perturb_state)
+                else:
+                    cvfs_out.add(perturb_state)
+    return {"cvfs_in": cvfs_in, "cvfs_out": cvfs_out}
 
 program_transitions_n_cvf = {}
 
 for state in configurations:
-    program_transitions_n_cvf[state] = get_program_transitions_cvfs(state)
+    program_transitions_n_cvf[state] = {**get_program_transitions(state), **get_cvfs(state)}
 
-# print("All Program transitions and CVFs:")
 
-unranked_states = set(program_transitions_n_cvf.keys()) - set(
-    program_transitions_rank.keys()
-)
-# print("Unranked states for Program transitions:")
-# unranked_states, len(unranked_states)
+unranked_states = set(program_transitions_n_cvf.keys()) - set(program_transitions_rank.keys())
+print("Unranked states for Program transitions:", len(unranked_states))
 
 # rank the states that has all the paths to the ranked one
 while unranked_states:
     ranked_states = set(program_transitions_rank.keys())
     remove_from_unranked_states = set()
     for state in unranked_states:
-        dests = program_transitions_n_cvf[state]["program_transitions"]
-        if dests - ranked_states:
+        dests = program_transitions_n_cvf[state]['program_transitions']
+        if dests - ranked_states:       # some desitnations states are yet to be ranked
             pass
-        else:
-            # all the destination has been ranked
+        else:                           # all the destination has been ranked
             total_path_length = 0
             path_count = 0
             _max = 0
             for succ in dests:
                 path_count += program_transitions_rank[succ]["C"]
-                total_path_length += (
-                    program_transitions_rank[succ]["L"]
-                    + program_transitions_rank[succ]["C"]
-                )
+                total_path_length += program_transitions_rank[succ]["L"] + program_transitions_rank[succ]["C"]
                 _max = max(_max, program_transitions_rank[succ]["M"])
             program_transitions_rank[state] = {
                 "L": total_path_length,
                 "C": path_count,
-                "A": total_path_length / path_count,
-                "Ar": round(total_path_length / path_count),
-                "M": _max + 1,
+                "A": total_path_length/path_count,
+                "Ar": math.ceil(total_path_length/path_count),
+                "M": _max + 1
             }
             remove_from_unranked_states.add(state)
     unranked_states -= remove_from_unranked_states
 
 
 pt_rank_effect = {}
-for state, transition_cvfs in program_transitions_n_cvf.items():
-    for pt in transition_cvfs["program_transitions"]:
+
+for state, pt_cvfs in program_transitions_n_cvf.items():
+    for pt in pt_cvfs['program_transitions']:
         pt_rank_effect[(state, pt)] = {
-            "A": program_transitions_rank[pt]["A"]
-            - program_transitions_rank[state]["A"],
-            "M": program_transitions_rank[pt]["M"]
-            - program_transitions_rank[state]["M"],
+            "Ar": program_transitions_rank[pt]["Ar"] - program_transitions_rank[state]["Ar"],
+            "M": program_transitions_rank[pt]["M"] - program_transitions_rank[state]["M"]
         }
-        pt_rank_effect[(state, pt)]["Ar"] = round(pt_rank_effect[(state, pt)]["A"])
 
 
-cvfs_rank = {}
-for state, transition_cvfs in program_transitions_n_cvf.items():
-    for cvf in transition_cvfs["cvfs"]:
-        cvfs_rank[(state, cvf)] = {
-            "A": program_transitions_rank[cvf]["A"]
-            - program_transitions_rank[state]["A"],
-            "M": program_transitions_rank[cvf]["M"]
-            - program_transitions_rank[state]["M"],
+cvfs_in_rank_effect = {}
+cvfs_out_rank_effect = {}
+
+for state, pt_cvfs in program_transitions_n_cvf.items():
+    for cvf in pt_cvfs['cvfs_in']:
+        cvfs_in_rank_effect[(state, cvf)] = {
+            "Ar": program_transitions_rank[cvf]["Ar"] - program_transitions_rank[state]["Ar"],
+            "M": program_transitions_rank[cvf]["M"] - program_transitions_rank[state]["M"]
         }
-        cvfs_rank[(state, cvf)]["Ar"] = round(cvfs_rank[(state, cvf)]["A"])
+    for cvf in pt_cvfs['cvfs_out']:
+        cvfs_out_rank_effect[(state, cvf)] = {
+            "Ar": program_transitions_rank[cvf]["Ar"] - program_transitions_rank[state]["Ar"],
+            "M": program_transitions_rank[cvf]["M"] - program_transitions_rank[state]["M"]
+        }
 
 
-fieldnames = ["State", "A", "Ar", "M"]
-with open(f"results/program_transitions_{graph_name}.csv", "w", newline="") as f:
+pt_rank_effect_ = []
+for state in pt_rank_effect:
+    pt_rank_effect_.append({"state": state, **pt_rank_effect[state]})
+
+pt_rank_effect_df = pd.DataFrame(pt_rank_effect_)
+
+cvfs_in_rank_effect_ = []
+for state in cvfs_in_rank_effect:
+    cvfs_in_rank_effect_.append({"state": state, **cvfs_in_rank_effect[state]})
+    
+cvfs_in_rank_effect_df = pd.DataFrame(cvfs_in_rank_effect_)
+
+cvfs_out_rank_effect_ = []
+for state in cvfs_out_rank_effect:
+    cvfs_out_rank_effect_.append({"state": state, **cvfs_out_rank_effect[state]})
+
+cvfs_out_rank_effect_df = pd.DataFrame(cvfs_out_rank_effect_)
+
+pt_avg_counts = pt_rank_effect_df['Ar'].value_counts()
+pt_max_counts = pt_rank_effect_df['M'].value_counts()
+cvf_in_avg_counts = cvfs_in_rank_effect_df['Ar'].value_counts()
+cvf_in_max_counts = cvfs_in_rank_effect_df['M'].value_counts()
+cvf_out_avg_counts = cvfs_out_rank_effect_df['Ar'].value_counts()
+cvf_out_max_counts = cvfs_out_rank_effect_df['M'].value_counts()
+
+fieldnames = ["Rank Effect", "PT (Max)", "PT (Avg)", "CVF In (Max)", "CVF In (Avg)", "CVF Out (Max)", "CVF Out (Avg)"]
+with open(os.path.join(results_dir, f"rank_effect_{graph_name}.csv"), "w", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
-    for state, rank in pt_rank_effect.items():
-        writer.writerow({"State": state, **rank})
+    for re in sorted(
+        set(pt_avg_counts.index) |
+        set(pt_max_counts.index) |
+        set(cvf_in_avg_counts.index) |
+        set(cvf_in_max_counts.index) |
+        set(cvf_out_avg_counts.index) |
+        set(cvf_out_max_counts.index)
+    ):
+        writer.writerow({
+            "Rank Effect": re,
+            "PT (Max)": pt_max_counts.get(re, 0),
+            "PT (Avg)": pt_avg_counts.get(re, 0),
+            "CVF In (Max)": cvf_in_max_counts.get(re, 0),
+            "CVF In (Avg)": cvf_in_avg_counts.get(re, 0),
+            "CVF Out (Max)": cvf_out_max_counts.get(re, 0),
+            "CVF Out (Avg)": cvf_out_avg_counts.get(re, 0),
+        })
 
 
-with open(f"results/cvf_{graph_name}.csv", "w", newline="") as f:
+pt_rank_ = []
+for state in program_transitions_rank:
+    pt_rank_.append({"state": state, **program_transitions_rank[state]})
+
+pt_rank_df = pd.DataFrame(pt_rank_)
+
+pt_avg_counts = pt_rank_df['Ar'].value_counts()
+pt_max_counts = pt_rank_df['M'].value_counts()
+
+fieldnames = ["Rank", "Count (Max)", "Count (Avg)"]
+with open(os.path.join(results_dir, f"rank_{graph_name}.csv"), "w", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
-    for state, rank in cvfs_rank.items():
-        writer.writerow({"State": state, **rank})
+    for rank in sorted(set(pt_avg_counts.index)|set(pt_max_counts.index)):
+        writer.writerow({"Rank": rank, "Count (Max)": pt_max_counts.get(rank, 0), "Count (Avg)": pt_avg_counts.get(rank, 0)})
+
+total_time = time.time() - start_time
+
+print(f"Took {total_time} (seconds).")
---
+++ submit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --account=cvf-analysis
+#SBATCH --time=10:00:00
+#SSBATCH --mail-type=ALL
+#SSBATCH --mail-user=agaru@uwyo.edu
+#SBATCH --output=cvf_%A.log
+#SBATCH --nodes=1
+#SBATCH --mem=1000G
+#SBATCH --partition=beartooth-hugemem
+
+
+echo "JobID:         "$SLURM_JOB_ID
+echo "JobName:       "$SLURM_JOB_NAME
+echo "Partition:     "$SLURM_JOB_PARTITION
+echo "Nodes:         "$SLURM_JOB_NUM_NODES
+echo "NodeList:      "$SLURM_JOB_NODELIST
+echo "Tasks per Node:"$SLURM_TASKS_PER_NODE
+echo "CPUs per Node: "$SLURM_JOB_CPUS_PER_NODE
+echo "CPUs per Task: "$SLURM_CPUS_PER_TASK
+echo "CPUs on Node:  "$SLURM_CPUS_ON_NODE
+echo "WorkingDir:    "$SLURM_SUBMIT_DIR
+
+echo "Loading Modules..."
+module load gcc/11.2.0 python/3.10.8
+
+echo "Running the script..."
+cd coloring
+python graph_coloring_cvf.py
+
+echo "Done!"
