4f97e1042e54c254468b6fd91f3c12ae090470ca
Amit Garu <agaru@uwyo.edu>
2024-03-21 16:46:53 -0600

    nohup sh files for running in internal servers

---
+++ nohup_commands.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+cd cvf-analysis
+python3 main.py --program maximal_matching --graph-names graph_1 
---
+++ run_nohup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+jobID=$(shuf -i 10000-99999 -n 1)
+echo "Job ID: ""$jobID"
+nohup ./nohup_commands.sh > cvf_"$jobID".log &
