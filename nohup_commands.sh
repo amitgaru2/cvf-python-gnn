a2893a0d76676f40772d38e85d4fad5b88b4d80a
Amit Garu <agaru@uwyo.edu>
2024-03-21 17:04:50 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+source venv/bin/activate
 cd cvf-analysis
-python3 main.py --program maximal_matching --graph-names graph_1 
+python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
