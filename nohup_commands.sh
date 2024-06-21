2f0b382114c402e8411da6370feb46cf891cb87e
Amit Garu <agaru@uwyo.edu>
2024-06-21 16:04:32 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -3,5 +3,5 @@ set -e
 source venv/bin/activate
 cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
-python3 main.py --program dijkstra_token_ring --graph-names 3 4 5 6 7 8 9 10 11 12
-python3 main.py --program dijkstra_token_ring -f --graph-names 3 4 5 6 7 8 9 10 11 12
+#python3 main.py --program dijkstra_token_ring --graph-names 13 14 15 16 17 18 19
+python3 main.py --program dijkstra_token_ring -f --graph-names 13 14 
