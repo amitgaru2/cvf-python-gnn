6ba465c38cbc70960575aa6d4b904d875ccc402d
Amit Garu <agaru@uwyo.edu>
2024-06-21 15:23:24 -0600

    dijkstra minor fix

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -3,3 +3,4 @@ set -e
 source venv/bin/activate
 cd cvf-analysis
 python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
+# python3 main.py --program dijkstra_token_ring --graph-names 3 4 5
