f510f06dcee995daaf6bca7565ae6f5f8f0cda35
Amit Garu <agaru@blog2.cluster>
2024-06-22 13:26:34 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -4,4 +4,11 @@ source venv/bin/activate
 cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
 #python3 main.py --program dijkstra_token_ring --graph-names 13 14 15 16 17 18 19
-python3 main.py --program dijkstra_token_ring -f --graph-names 13 14 
+python3 main.py --program maximal_matching --graph-names graph_1
+python3 main.py --program maximal_matching --graph-names graph_2
+python3 main.py --program maximal_matching --graph-names graph_3
+python3 main.py --program maximal_matching --graph-names graph_4
+python3 main.py --program maximal_matching --graph-names graph_5
+python3 main.py --program maximal_matching --graph-names graph_6
+python3 main.py --program maximal_matching --graph-names graph_7
+python3 main.py --program maximal_matching --graph-names graph_8
