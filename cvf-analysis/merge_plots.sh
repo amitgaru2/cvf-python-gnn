0445f7f78ac642bd16488666bec5dba898680677
Amit Garu <amitgaru2@gmail.com>
2024-06-29 12:21:07 -0600

    updates

--- cvf-analysis/merge_plots.sh
+++ cvf-analysis/merge_plots.sh
@@ -25,6 +25,8 @@ GRAPH_IMAGE_LOCATION="graph_images/${2}.png"
 NODE_EFFECT_PLOT_LOCATION="plots/${1}/rank_effect_by_node__full__${1}__${2}.png"
 # BASE_NODE_EFFECT_PLOT_SIZE=$(identify -ping -format '%[width]x%[height]' ${NODE_EFFECT_PLOT_LOCATION})
 
+GRAPH_IMAGE_LOCATION="graph_images/${2}.png"
+
 ls plots/${1}/rank_effect_by_node__full__${1}__${2}__node_*.png > "${NODE_EFFECTS_MERGE_PLOT_LIST}"
 
 
@@ -46,42 +48,46 @@ then
         #         -append merged_plots/${1}_merged_plot_${2}.png
 
         montage $(cat $NODE_EFFECTS_MERGE_PLOT_LIST) \
-                graph_images/${2}.png \
+                ${GRAPH_IMAGE_LOCATION} \
                 ${NODE_DEGREE_VS_RANK_EFFECT_PLOTS_LOCATION}/node_degree_vs_rank_effect__full__${1}__${2}.png \
                 ${NODE_VS_CVF_PLOTS_LOCATION}/node__vs__rank_effect_gte_*__full__${1}__${2}.png \
                 -tile 3x \
                 -geometry +10+10 \
                 merged_plots/${1}_merged_plot_${2}.png
+
         exit 0
 fi
 
 
 ls -r ${NODE_VS_ACCUMULATED_CVF_PLOTS_LOCATION}/node__vs__accumulated_severe_cvf_effect_gte_*__full__${1}__${2}.png > "${NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST}"
 # # horizontally append
-convert $(cat $NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST) +append ${NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_PLOT}
+# convert $(cat $NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST) +append ${NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_PLOT}
 
 # # horizontally append
 # convert ${NODE_VS_MAX_CVF_PLOTS_LOCATION}/node_vs_max_rank_effect__full__${1}__${2}.png \
 #         ${NODE_VS_CVF_PLOTS_LOCATION}/node__vs__rank_effect_gte_*__full__${1}__${2}.png \
 #         +append ${NODE_VS_MAX_CVF__CVF__ACCUMULATED_CVF_MERGE_PLOT}
 
-# if [[ "$1" == "dijkstra_token_ring" ]];
-# then
-# echo "Dijkstra skipping appending graph image"
-# else
-# # horizontally append
-# # resize to base image
-# # NODE_MAX_CVF_PLOT_SIZE=$(identify -ping -format '%[width]x%[height]' ${NODE_VS_MAX_CVF_PLOTS_LOCATION}/node_vs_max_rank_effect__full__${1}__${2}.png)
-
-# # convert ${GRAPH_IMAGE_LOCATION} \
-# #         -resize ${NODE_MAX_CVF_PLOT_SIZE} \
-# #         ${GRAPH_IMAGE_LOCATION}
-
-# convert ${GRAPH_IMAGE_LOCATION} \
-#         ${NODE_VS_MAX_CVF__CVF__ACCUMULATED_CVF_MERGE_PLOT} \
-#         -gravity center \
-#         +append ${NODE_VS_MAX_CVF__CVF__ACCUMULATED_CVF_MERGE_PLOT}
-# fi
+if [[ "$1" == "dijkstra_token_ring" ]];
+then
+echo "Dijkstra skipping appending graph image"
+montage $(cat $NODE_EFFECTS_MERGE_PLOT_LIST) \
+        ${NODE_VS_MAX_CVF_PLOTS_LOCATION}/node_vs_max_rank_effect__full__${1}__${2}.png \
+        ${NODE_VS_CVF_PLOTS_LOCATION}/node__vs__rank_effect_gte_*__full__${1}__${2}.png \
+        $(cat $NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST) \
+        -tile 3x \
+        -geometry +10+10 \
+        merged_plots/${1}_merged_plot_${2}.png
+else
+montage $(cat $NODE_EFFECTS_MERGE_PLOT_LIST) \
+        ${GRAPH_IMAGE_LOCATION} \
+        ${NODE_VS_MAX_CVF_PLOTS_LOCATION}/node_vs_max_rank_effect__full__${1}__${2}.png \
+        ${NODE_VS_CVF_PLOTS_LOCATION}/node__vs__rank_effect_gte_*__full__${1}__${2}.png \
+        $(cat $NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST) \
+        -tile 3x \
+        -geometry +10+10 \
+        merged_plots/${1}_merged_plot_${2}.png
+fi
 
 
 # # vertically append
@@ -102,3 +108,4 @@ convert $(cat $NODE_VS_ACCUMULATED_CVF_PLOTS_MERGE_FILELIST) +append ${NODE_VS_A
 
 # # rm merged_plots/max_matching_results_graph_6.png
 # # rm ${ACCUMULATED_CVF_PLOTS_MERGE_FILELIST}
+
