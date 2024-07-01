444196f963f7611e84abfdd20d89c73d8197e5f3
Amit Garu <agaru@uwyo.edu>
2024-07-01 15:57:22 -0600

    updates

--- cvf-analysis/plot_graphs.py
+++ cvf-analysis/plot_graphs.py
@@ -3,18 +3,18 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 graph_names = [
-    "graph_1",
-    "graph_2",
-    "graph_3",
-    "graph_6",
-    "graph_6b",
-    "graph_7",
-    "graph_8",
-    # "graph_4",
-    # "graph_5",
+    # "graph_1",
+    # "graph_2",
+    # "graph_3",
+    # "graph_6",
+    # "graph_6b",
+    # "graph_7",
+    # "graph_8",
+    "graph_4",
+    "graph_5",
 ]
 
-planar = False
+planar = True
 fontsize = 20
 
 for gname in graph_names:
@@ -22,7 +22,7 @@ for gname in graph_names:
     fig = plt.figure(figsize=(10, 5), constrained_layout=True)
     if planar:
         pos = nx.planar_layout(G)
-        nx.draw_networkx(G, pos=pos, node_color="black", ax=fig.add_subplot(), font_size=25)
+        nx.draw_networkx(G, pos=pos, node_color="black", font_color="white", ax=fig.add_subplot(), font_size=30, node_size=1200)
     else:
         nx.draw_networkx(G, node_color="black", font_color="white", ax=fig.add_subplot(), font_size=30, node_size=1200)
 
