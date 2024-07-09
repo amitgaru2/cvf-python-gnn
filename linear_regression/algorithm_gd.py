8662151e57b968b037a21af0ad51b758944db806
Amit Garu <agaru@uwyo.edu>
2024-07-09 15:39:20 -0600

    updates

--- linear_regression/algorithm_gd.py
+++ linear_regression/algorithm_gd.py
@@ -1,46 +1,63 @@
-def forward(x, m, c):
-    return [m * i + c for i in x]
+def forward(X, params):
+    return [params["m"] * i + params["c"] for i in X]
 
 
-def r2_score(y_actual, y_actual_mean, y_pred):
-    N = len(y_actual)
-    rss = sum((y_actual[i] - y_pred[i]) ** 2 for i in range(N))
-    tss = sum((y_actual[i] - y_actual_mean) ** 2 for i in range(N))
+def loss_fn(y, y_pred):
+    N = len(y)
+    return (1 / N) * sum((y[i] - y_pred[i]) ** 2 for i in range(N))
+
+
+def r2_score(y, y_mean, y_pred):
+    N = len(y)
+    rss = sum((y[i] - y_pred[i]) ** 2 for i in range(N))
+    tss = sum((y[i] - y_mean) ** 2 for i in range(N))
     r2 = 1 - rss / tss
     return r2
 
 
-def gradient_m(x, y_actual, y_pred):
-    N = len(y_actual)
-    return (-2 / N) * sum((x[i] * (y_actual[i] - y_pred[i])) for i in range(N))
+def gradient_m(X, y, y_pred):
+    N = len(y)
+    return (-2 / N) * sum((X[i] * (y[i] - y_pred[i])) for i in range(N))
 
 
-def gradient_c(y_actual, y_pred):
-    N = len(y_actual)
-    return (-2 / N) * sum((y_actual[i] - y_pred[i]) for i in range(N))
+def gradient_c(y, y_pred):
+    N = len(y)
+    return (-2 / N) * sum((y[i] - y_pred[i]) for i in range(N))
 
 
-def get_iteration_vs_accuracy_data(x, y, iterations, L=0.0001, m=0, c=0):
-    y_mean = sum(y) / len(y)
+def get_iteration_vs_accuracy_data(
+    X, y, iterations, X_test=None, y_test=None, L=0.0001, m=0, c=0
+):
+    if X_test is None:
+        X_test = X
+        y_test = y
+
+    # y_mean = sum(y) / len(y)
+    y_test_mean = sum(y_test) / len(y_test)
 
     # initial guess
     m = 0
     c = 0
+
+    params = {"m": m, "c": c}
 
     steps_data = []
     accuracy_data = []
 
     for i in range(1, iterations + 1):
         steps_data.append(i)
-        y_pred = forward(x, m, c)
-        accuracy = r2_score(y, y_mean, y_pred)
-        accuracy_data.append(accuracy)
-        # print("Loss:", loss, "| Accuracy:", accuracy)
+        y_pred = forward(X, params)
+        y_test_pred = forward(X_test, params)
 
-        grad_m = gradient_m(x, y, y_pred)
+        accuracy = r2_score(y_test, y_test_mean, y_test_pred)
+        accuracy_data.append(accuracy)
+
+        grad_m = gradient_m(X, y, y_pred)
         grad_c = gradient_c(y, y_pred)
 
         m = m - L * grad_m
         c = c - L * grad_c
+
+        params = {"m": m, "c": c}
 
     return steps_data, accuracy_data
