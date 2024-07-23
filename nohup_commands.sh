816862cc6f0fbca2527427919ab3501c0dafdbca
Amit Garu <agaru@uwyo.edu>
2024-07-23 11:35:20 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -4,11 +4,12 @@ source venv/bin/activate
 cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
 #python3 main.py --program dijkstra_token_ring --graph-names 13 14 15 16 17 18 19
-python3 main.py --program maximal_matching --graph-names graph_1
-python3 main.py --program maximal_matching --graph-names graph_2
-python3 main.py --program maximal_matching --graph-names graph_3
-python3 main.py --program maximal_matching --graph-names graph_4
-python3 main.py --program maximal_matching --graph-names graph_5
-python3 main.py --program maximal_matching --graph-names graph_6
-python3 main.py --program maximal_matching --graph-names graph_7
-python3 main.py --program maximal_matching --graph-names graph_8
+#python3 main.py --program maximal_matching --graph-names graph_1
+#python3 main.py --program maximal_matching --graph-names graph_2
+#python3 main.py --program maximal_matching --graph-names graph_3
+#python3 main.py --program maximal_matching --graph-names graph_4
+#python3 main.py --program maximal_matching --graph-names graph_5
+#python3 main.py --program maximal_matching --graph-names graph_6
+#python3 main.py --program maximal_matching --graph-names graph_7
+#python3 main.py --program maximal_matching --graph-names graph_8
+python3 main.py --program linear_regression -f graph_names test_lr_graph_2
