671c0144427d9af4a560c5dad5a32c730cb90caa
Amit Garu <agaru@uwyo.edu>
2024-08-27 16:18:01 -0600

    updates

--- cvf-analysis/linear_regression.py
+++ cvf-analysis/linear_regression.py
@@ -20,39 +20,39 @@ class LinearRegressionFullAnalysis(CVFAnalysis):
 
         self.iterations = 10
 
-        # self.learning_rate = 0.001
-        # self.slope_step_decimals = 1
-        # self.min_slope = np.float64(0.0)
-        # self.max_slope = np.float64(1.1)
-        # self.no_of_nodes = 3
-        # self.df = pd.read_csv(
-        #     "/home/agaru/research/cvf-python/linear_regression/random-data.csv"
-        # )
-        # self.doubly_stochastic_matrix_config = [
-        #     [1 / 3, 1 / 3, 1 / 3],
-        #     [1 / 3, 2 / 3, 0],
-        #     [1 / 3, 0, 2 / 3],
-        # ]
+        self.learning_rate = 0.001
+        self.slope_step_decimals = 1
+        self.min_slope = np.float64(0.0)
+        self.max_slope = np.float64(1.1)
+        self.no_of_nodes = 3
+        self.df = pd.read_csv(
+            "/home/agaru/research/cvf-python/linear_regression/random-data.csv"
+        )
+        self.doubly_stochastic_matrix_config = [
+            [1 / 3, 1 / 3, 1 / 3],
+            [1 / 3, 2 / 3, 0],
+            [1 / 3, 0, 2 / 3],
+        ]
         # self.actual_m = 0.9
         # self.actual_b = -0.11847322643445737
 
-        self.learning_rate = 0.0001
-        self.slope_step_decimals = 1
-        self.min_slope = np.float64(0.0)
-        self.max_slope = np.float64(2.0)
-        self.no_of_nodes = 4
-        self.df = pd.read_csv(
-            "/home/amitgaru2/research/cvf-python/linear_regression/SOCR-HeightWeight.csv"
-        )
-        self.df.rename(
-            columns={"Height(Inches)": "X", "Weight(Pounds)": "y"}, inplace=True
-        )
-        self.doubly_stochastic_matrix_config = [
-            [1 / 2, 1 / 4, 1 / 8, 1 / 8],
-            [1 / 4, 3 / 4, 0, 0],
-            [1 / 8, 0, 7 / 8, 0],
-            [1 / 8, 0, 0, 7 / 8],
-        ]
+        # self.learning_rate = 0.0001
+        # self.slope_step_decimals = 1
+        # self.min_slope = np.float64(1.0)
+        # self.max_slope = np.float64(2.0)
+        # self.no_of_nodes = 4
+        # self.df = pd.read_csv(
+        #     "/home/agaru/research/cvf-python/linear_regression/SOCR-HeightWeight.csv"
+        # )
+        # self.df.rename(
+        #     columns={"Height(Inches)": "X", "Weight(Pounds)": "y"}, inplace=True
+        # )
+        # self.doubly_stochastic_matrix_config = [
+        #     [1 / 2, 1 / 4, 1 / 8, 1 / 8],
+        #     [1 / 4, 3 / 4, 0, 0],
+        #     [1 / 8, 0, 7 / 8, 0],
+        #     [1 / 8, 0, 0, 7 / 8],
+        # ]
         # self.actual_m = 3.08
 
         self.slope_step = 1 / (10**self.slope_step_decimals)
@@ -85,11 +85,11 @@ class LinearRegressionFullAnalysis(CVFAnalysis):
         self._init_pts_rank()
         self.__save_pts_to_file()
         self._rank_all_states()
-        self._gen_save_rank_count()
-        self._calculate_pts_rank_effect()
-        self._calculate_cvfs_rank_effect()
-        self._gen_save_rank_effect_count()
-        self._gen_save_rank_effect_by_node_count()
+        # self._gen_save_rank_count()
+        # self._calculate_pts_rank_effect()
+        # self._calculate_cvfs_rank_effect()
+        # self._gen_save_rank_effect_count()
+        # self._gen_save_rank_effect_by_node_count()
 
     def _gen_configurations(self):
         self.configurations = {tuple([self.min_slope for _ in range(len(self.nodes))])}
@@ -172,113 +172,6 @@ class LinearRegressionFullAnalysis(CVFAnalysis):
         lst_copy[indx] = value
         return lst_copy
 
-    def _find_program_transitions(self):
-        node_params = [self.min_slope for i in range(self.no_of_nodes)]
-        program_transitions = []
-        for i in range(1, self.iterations + 1):
-            prev_node_params = node_params.copy()
-            for node_id in range(self.no_of_nodes):
-                m_node = node_params[node_id]
-
-                node_df = self.__get_node_data_df(node_id)
-                X_node = node_df["X"].array
-                y_node = node_df["y"].array
-
-                y_node_pred = self.__forward(X_node, {"m": m_node, "c": 0})
-                grad_m = self.__gradient_m(X_node, y_node, y_node_pred)
-
-                doubly_st_mt = self.doubly_stochastic_matrix_config[node_id]
-
-                node_params[node_id] = (
-                    sum(
-                        frac * prev_node_params[i]
-                        for i, frac in enumerate(doubly_st_mt)
-                    )
-                    - self.learning_rate * grad_m
-                )
-
-            program_transitions.append(prev_node_params)
-
-        # find step by given self.slope_step; remove redundant state transitions
-        actual_program_transitions = []
-        prev_trans = program_transitions[0]
-        prev_trans_cleaned = self.__clean_float_to_step_size(prev_trans)
-        actual_program_transitions.append(prev_trans_cleaned)
-        for trans in program_transitions[1:]:
-            trans_cleaned = self.__clean_float_to_step_size(trans)
-            if prev_trans_cleaned != trans_cleaned:
-                actual_program_transitions.append(trans_cleaned)
-            prev_trans_cleaned = trans_cleaned
-
-        pprint.pprint(actual_program_transitions)
-        print(len(actual_program_transitions))
-
-    def _find_program_transitions_v2(self):
-        searched_states = set()
-        program_transitions = {
-            tuple([self.min_slope for i in range(self.no_of_nodes)]): []
-        }
-        actual_program_transitions = []
-        left_tobe_searched_node_params = program_transitions.keys() - searched_states
-        while left_tobe_searched_node_params:
-            actual_node_params = list(left_tobe_searched_node_params)[0]
-            node_params = list(actual_node_params)
-            for i in range(1, self.iterations + 1):
-                prev_node_params = node_params.copy()
-                for node_id in range(self.no_of_nodes):
-                    m_node = prev_node_params[node_id]
-
-                    node_df = self.__get_node_data_df(node_id)
-                    X_node = node_df["X"].array
-                    y_node = node_df["y"].array
-
-                    y_node_pred = self.__forward(X_node, {"m": m_node, "c": 0})
-                    grad_m = self.__gradient_m(X_node, y_node, y_node_pred)
-
-                    doubly_st_mt = self.doubly_stochastic_matrix_config[node_id]
-
-                    new_slope = (
-                        sum(
-                            frac * prev_node_params[j]
-                            for j, frac in enumerate(doubly_st_mt)
-                        )
-                        - self.learning_rate * grad_m
-                    )
-                    new_slope_cleaned = self.__clean_float_to_step_size_single(
-                        new_slope
-                    )
-                    if new_slope_cleaned != self.__clean_float_to_step_size_single(
-                        node_params[node_id]
-                    ):
-                        new_node_params = self.__copy_replace_indx_value(
-                            prev_node_params, node_id, new_slope_cleaned
-                        )
-                        new_node_params = tuple(
-                            self.__clean_float_to_step_size(new_node_params)
-                        )
-                        actual_program_transitions.append(new_node_params)
-                    else:
-                        node_params[node_id] = new_slope
-
-                if actual_program_transitions:
-                    program_transitions[actual_node_params].extend(
-                        actual_program_transitions
-                    )
-                    for pt in actual_program_transitions:
-                        if pt not in program_transitions:
-                            program_transitions[pt] = []
-                    actual_program_transitions = []
-                    break
-            else:
-                logger.debug("No program transition found for %s !", actual_node_params)
-
-            searched_states.add(actual_node_params)
-            left_tobe_searched_node_params = (
-                program_transitions.keys() - searched_states
-            )
-
-        self._temp_program_transitions = program_transitions
-
     def _get_program_transitions(self, start_state):
         program_transitions = set()
 
@@ -332,27 +225,6 @@ class LinearRegressionFullAnalysis(CVFAnalysis):
         # logger.debug("%s : %s", start_state, program_transitions)
 
         return program_transitions
-
-    def __newton_raphson_L(self, L0, m_pred, m, grad_m):
-        def f_L(L):
-            return m_pred - m + L * grad_m
-
-        def f_L_der(L):
-            return grad_m
-
-        numerator = f_L(L0)
-        denominator = f_L_der(L0)
-
-        while numerator != 0:
-            new_L0 = L0 - numerator / denominator
-            numerator = f_L(new_L0)
-            denominator = f_L_der(new_L0)
-            if abs(new_L0 - L0) <= 0.00001:
-                L0 = new_L0
-                break
-            L0 = new_L0
-
-        return L0
 
     def _get_cvfs(self, start_state):
         cvfs = dict()
