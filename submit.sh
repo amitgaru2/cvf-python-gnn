0039bb3c824f12b74c8af6e2188aa4d2e7651c45
Amit Garu <agaru@blog2.cluster>
2024-09-15 08:04:00 -0600

    updates

--- submit.sh
+++ submit.sh
@@ -33,9 +33,21 @@ cd 'cvf-analysis'
 #python main.py --program maximal_matching --graph-names graph_1 graph_2 graph_3 graph_4 graph_5 graph_6 graph_7 graph_8
 #python main.py --program graph_coloring -f --graph-names graph_6b
 #python main.py --program maximal_matching -f --graph-names graph_6b
-python main.py -f --program maximal_independent_set --graph-names graph_1 graph_2 graph_3 graph_4 graph_5 graph_6 graph_7 graph_8 graph_6b
+#python main.py -f --program maximal_independent_set --graph-names graph_1 graph_2 graph_3 graph_4 graph_5 graph_6 graph_7 graph_8 graph_6b
 #python main.py --program graph_coloring --graph-names graph_3 graph_4 graph_5 graph_8
 #python main.py --program maximal_matching --graph_names graph_1 --sample-size 10000
 #python main.py --program maximal_matching --graph_names graph_1 graph_2 graph_3 graph_4 graph_5 graph_6 graph_7 graph_8
+
+#python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
+#python3 main.py --program dijkstra_token_ring --graph-names 13 14 15 16 17 18 19
+#python3 main.py --program maximal_matching --graph-names graph_1
+#python3 main.py --program maximal_matching --graph-names graph_2
+#python3 main.py --program maximal_matching --graph-names graph_3
+#python3 main.py --program maximal_matching --graph-names graph_4
+#python3 main.py --program maximal_matching --graph-names graph_5
+#python3 main.py --program maximal_matching --graph-names graph_6
+#python3 main.py --program maximal_matching --graph-names graph_7
+#python3 main.py --program maximal_matching --graph-names graph_8
+python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG
 
 echo "Done!"
