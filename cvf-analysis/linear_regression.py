c8d4c9ac4b468e69f967ae16e61b8f592474d54b
Amit Garu <agaru@uwyo.edu>
2024-09-17 11:30:41 -0600

    updates

--- cvf-analysis/linear_regression.py
+++ cvf-analysis/linear_regression.py
@@ -50,7 +50,7 @@ class LinearRegressionFullAnalysis(CVFAnalysis):
         self.stop_threshold = 0.0001
         self.slope_step = np.float64(0.025)
         self.slope_step_decimals = 3
-        self.min_slope = np.float64(0.800)
+        self.min_slope = np.float64(1.000)
         self.max_slope = np.float64(1.900)
         # self.no_of_nodes = 4
         self.df = pd.read_csv(
