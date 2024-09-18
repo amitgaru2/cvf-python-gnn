463dc7494cefb1fc4a5ccd0c33e7f92468626653
Amit Garu <agaru@blog2.cluster>
2024-09-18 08:57:23 -0600

    updates

--- cvf-analysis/lr_configs/matrix_5.py
+++ cvf-analysis/lr_configs/matrix_5.py
@@ -6,7 +6,7 @@ learning_rate = 0.0001
 stop_threshold = 0.0001
 slope_step = np.float64(0.025)
 slope_step_decimals = 3
-min_slope = np.float64(1.100)
+min_slope = np.float64(1.400)
 max_slope = np.float64(1.900)
 
 data_path = os.path.join(
