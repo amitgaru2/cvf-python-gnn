3148745bc15cc4fc1a276d5e73bf2399aad98f29
Amit Garu <amitgaru2@gmail.com>
2024-09-24 01:44:16 -0600

    updates

--- cvf-analysis/plot_graphs.py
+++ cvf-analysis/plot_graphs.py
@@ -8,11 +8,12 @@ graph_names = [
     # "graph_3",
     # "graph_6",
     # "graph_6b",
-    # "graph_7",
+    "graph_7",
     # "graph_8",
     # "graph_4",
     # "graph_5",
     "test_lr_graph_6",
+    # "implicit_graph_n5",
 ]
 
 planar = False
@@ -20,12 +21,14 @@ fontsize = 25
 
 for gname in graph_names:
     G = nx.read_adjlist(f"graphs/{gname}.txt")
-    fig = plt.figure(figsize=(10, 5), constrained_layout=True)
+    fig = plt.figure(figsize=(14, 7), constrained_layout=True)
     if planar:
         pos = nx.planar_layout(G)
         nx.draw_networkx(G, pos=pos, node_color="black", font_color="white", ax=fig.add_subplot(), font_size=30, node_size=1200)
     else:
-        nx.draw_networkx(G, node_color="black", font_color="white", ax=fig.add_subplot(), font_size=30, node_size=1200)
+        nx.draw_networkx(G, node_color="white", font_color="black", ax=fig.add_subplot(), font_size=60, node_size=5000)
+        ax = plt.gca() # to get the current axis
+        ax.collections[0].set_edgecolor("#000000") 
 
     # fig.suptitle(f"Graph {gname.split("_")[1]}", fontsize=fontsize)
     fig.savefig(f"graph_images/{gname}.png")
