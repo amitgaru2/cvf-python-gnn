99c6e3850da7e5c71ca17649e6ed02dd3306c57c
Amit Garu <agaru@uwyo.edu>
2024-09-25 12:09:15 -0600

    updates

--- submit.sh
+++ submit.sh
@@ -50,6 +50,7 @@ cd 'cvf-analysis'
 #python3 main.py --program maximal_matching --graph-names graph_8
 #python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG --config-file matrix_4
 #python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG --config-file matrix_5
-python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG --config-file matrix_6
+# python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG --config-file matrix_6
+python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG --config-file matrix_7
 
 echo "Done!"
