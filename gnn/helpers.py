96c43dd4a42c5f6c5b020f3bf80a1fc76d3b3102
Amit Garu <agaru@uwyo.edu>
2024-11-13 12:46:59 -0700

    updates

--- gnn/helpers.py
+++ gnn/helpers.py
@@ -5,10 +5,11 @@ from torch.utils.data import Dataset
 
 class CVFConfigDataset(Dataset):
     def __init__(self) -> None:
-        self.data = pd.read_csv()
+        self.data = pd.read_csv("small_graph_test_config_rank_dataset.csv")
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        pass
+        row = self.data.loc[idx]
+        return row.config, row.rank
