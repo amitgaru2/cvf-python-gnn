b0d45a16f2581e5be7ca6b1a728196719090425d
Amit Garu <agaru@uwyo.edu>
2024-11-13 12:58:31 -0700

    updates

--- gnn/helpers.py
+++ gnn/helpers.py
@@ -1,6 +1,8 @@
+import ast
 import pandas as pd
 
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
 
 class CVFConfigDataset(Dataset):
@@ -12,4 +14,13 @@ class CVFConfigDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.data.loc[idx]
-        return row.config, row.rank
+        return ast.literal_eval(row["config"]), row["rank"]
+
+
+if __name__ == "__main__":
+    dataset = CVFConfigDataset()
+    loader = DataLoader(dataset, batch_size=2, shuffle=False)
+
+    for dl in loader:
+        print(dl)
+        print()
