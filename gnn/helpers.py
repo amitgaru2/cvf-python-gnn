ba7c6841e502dd6338b9cc303514e661342dc79f
Amit Garu <agaru@uwyo.edu>
2024-11-18 14:24:51 -0700

    updates

--- gnn/helpers.py
+++ gnn/helpers.py
@@ -1,5 +1,6 @@
 import os
 import ast
+import json
 
 import torch
 import pandas as pd
@@ -9,9 +10,13 @@ from torch.utils.data import DataLoader
 
 
 class CVFConfigDataset(Dataset):
-    def __init__(self, dataset_file) -> None:
+    def __init__(self, dataset_file, edge_index_file) -> None:
         self.data = pd.read_csv(os.path.join("datasets", dataset_file))
         self.nodes = len(self[0][0])
+        self.edge_index = torch.tensor(
+            json.load(open(os.path.join("datasets", edge_index_file), "r")),
+            dtype=torch.long,
+        )
 
     def __len__(self):
         return len(self.data)
@@ -28,7 +33,9 @@ class CVFConfigDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = CVFConfigDataset("graph_4_config_rank_dataset.csv")
+    dataset = CVFConfigDataset(
+        "graph_4_config_rank_dataset.csv", "graph_4_edge_index.json"
+    )
     loader = DataLoader(dataset, batch_size=2, shuffle=False)
 
     print(dataset.nodes)
