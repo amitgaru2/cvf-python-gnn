a68c02d613b177412fcde290c713b3752fb834d9
Amit Garu <agaru@uwyo.edu>
2024-11-19 12:49:22 -0700

    sparse matrix for each color as input

--- gnn/helpers.py
+++ gnn/helpers.py
@@ -11,14 +11,14 @@ from torch.utils.data import DataLoader
 
 
 class CVFConfigDataset(Dataset):
-    def __init__(self, dataset_file, edge_index_file) -> None:
+    def __init__(self, dataset_file, edge_index_file, num_classes) -> None:
         self.data = pd.read_csv(os.path.join("datasets", dataset_file))
         # self.nodes = len(self[0][0])
         self.edge_index = torch.tensor(
             json.load(open(os.path.join("datasets", edge_index_file), "r")),
             dtype=torch.long,
         )
-        self.num_classes = 10
+        self.num_classes = num_classes
 
     def __len__(self):
         return len(self.data)
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     #     "graph_4_config_rank_dataset.csv", "graph_4_edge_index.json"
     # )
     dataset = CVFConfigDataset(
-        "small_graph_test_config_rank_dataset.csv", "small_graph_edge_index.json"
+        "small_graph_test_config_rank_dataset.csv", "small_graph_edge_index.json", 4
     )
     loader = DataLoader(dataset, batch_size=2, shuffle=False)
 
