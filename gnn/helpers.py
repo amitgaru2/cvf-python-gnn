b7cf0481f6201657b14ff7fababb076b798876b7
Amit Garu <agaru@uwyo.edu>
2024-11-26 14:03:35 -0700

    updates

--- gnn/helpers.py
+++ gnn/helpers.py
@@ -42,6 +42,36 @@ class CVFConfigDataset(Dataset):
         return result
 
 
+class CVFConfigForGCNDataset(Dataset):
+    def __init__(self, device, dataset_file, edge_index_file, num_classes) -> None:
+        self.data = pd.read_csv(os.path.join("datasets", dataset_file))
+        self.edge_index = (
+            torch.tensor(
+                json.load(open(os.path.join("datasets", edge_index_file), "r")),
+                dtype=torch.long,
+            )
+            .t()
+            .to(device)
+        )
+        self.num_classes = num_classes
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data.loc[idx]
+
+        result = (
+            F.one_hot(
+                torch.tensor(ast.literal_eval(row["config"])),
+                num_classes=self.num_classes,
+            ).to(torch.float32),
+            row["rank"],
+        )
+
+        return result
+
+
 if __name__ == "__main__":
     # dataset = CVFConfigDataset(
     #     "graph_4_config_rank_dataset.csv", "graph_4_edge_index.json"
