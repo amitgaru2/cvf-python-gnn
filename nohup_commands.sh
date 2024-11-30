196be4ac70722fa18b735ab87127a9f98e27df64
Amit Garu <agaru@uwyo.edu>
2024-11-29 20:37:59 -0700

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # source venv/bin/activate
-cd cvf-analysis
+# cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
 #python3 main.py --program dijkstra_token_ring --graph-names 13 14 15 16 17 18 19
 #python3 main.py --program maximal_matching --graph-names graph_1
@@ -13,5 +13,7 @@ cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_7
 #python3 main.py --program maximal_matching --graph-names graph_8
 # python3 main.py --program linear_regression -f --graph-names test_lr_graph_1 --logging DEBUG
-python graph_coloring_v2.py
+# python graph_coloring_v2.py
 
+cd gnn
+python vanilla_gcn_generic.py
