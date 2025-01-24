da52da96d9d4f3aa24a87a47448029b5b1f3baaf
Amit Garu <amitgaru2@gmail.com>
2025-01-24 09:44:44 -0700

    updates

--- cvf-analysis/custom_logger.py
+++ cvf-analysis/custom_logger.py
@@ -4,9 +4,7 @@ import datetime
 
 logger = logging.getLogger()
 
-filename = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S_log.txt")
-fh = logging.FileHandler(filename)
 
-# logger.addHandler(logging.StreamHandler())
-logger.addHandler(fh)
+logger.addHandler(logging.StreamHandler())
+# logger.addHandler(fh)
 logger.setLevel(logging.INFO)
