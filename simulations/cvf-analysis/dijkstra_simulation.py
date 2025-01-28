9523890cbaa01cc446c1c90db7ba404d906b8075
Amit Garu <amitgaru2@gmail.com>
2025-01-28 15:44:12 -0700

    updates

--- simulations/cvf-analysis/dijkstra_simulation.py
+++ simulations/cvf-analysis/dijkstra_simulation.py
@@ -49,16 +49,4 @@ class DijkstraSimulation(SimulationMixin, DijkstraTokenRingCVFAnalysisV2):
                     )
                 )
 
-        # for position, color in enumerate(state):
-        #     # check if node already has different color among the neighbors => If yes => not eligible to do anything
-        #     neighbor_colors = set(state[i] for i in self.graph[position])
-        #     if color not in neighbor_colors:  # is different color
-        #         # considering the case where if the node has different color than neighboring node, regardless minimum or not, then it is not eligible
-        #         continue
-        #     transition_color = self._find_min_possible_color(neighbor_colors)
-        #     if color != transition_color:
-        #         eligible_actions.append(
-        #             Action(Action.UPDATE, position, [color, transition_color])
-        #         )
-
         return eligible_actions
