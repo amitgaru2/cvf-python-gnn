d22e3053242f60d996934dee1e49a17bfbc975a8
Amit Garu <agaru@uwyo.edu>
2025-01-30 16:20:41 -0700

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 # source venv/bin/activate
 # cd cvf-analysis
 #python3 main.py --program maximal_matching --graph-names graph_5 graph_6 graph_7 graph_8
@@ -22,9 +23,9 @@ cd simulations/cvf-analysis
 #python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n30
 #python main.py --program graph_coloring --sched 1 --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n30
 #python main.py --program graph_coloring --sched 1 -me --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n30
-#python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
-#python main.py --program graph_coloring --sched 1 --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
-#python main.py --program graph_coloring --sched 1 -me --no-sim 100000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
-python main.py --program dijkstra_token_ring --sched 0 --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
-python main.py --program dijkstra_token_ring --sched 1 --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
-python main.py --program dijkstra_token_ring --sched 1 -me --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
+python main.py --program graph_coloring --sched 0 --no-sim 1000000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
+python main.py --program graph_coloring --sched 1 --no-sim 1000000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
+python main.py --program graph_coloring --sched 1 -me --no-sim 1000000 --fault-prob 0.5 --graph-names graph_powerlaw_cluster_graph_n29
+#python main.py --program dijkstra_token_ring --sched 0 --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
+#python main.py --program dijkstra_token_ring --sched 1 --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
+#python main.py --program dijkstra_token_ring --sched 1 -me --no-sim 50000 --fault-prob 0.5 --graph-names implicit_graph_n20 
