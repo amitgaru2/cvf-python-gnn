29215e63cf96e1114769f4680c4d25ee171e50bd
Amit Garu <agaru@uwyo.edu>
2025-02-18 20:02:21 -0700

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -33,9 +33,9 @@ cd simulations/cvf-analysis
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 2 --graph-names graph_7 --fault-prob 1.0
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 4 --graph-names graph_7 --fault-prob 1.0
-python main.py --program graph_coloring --sched 0 --no-sim 500000 --fault-interval 1 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
-python main.py --program graph_coloring --sched 0 --no-sim 500000 --fault-interval 2 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
-python main.py --program graph_coloring --sched 0 --no-sim 500000 --fault-interval 4 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
+# python main.py --program graph_coloring --sched 0 --no-sim 500000 --fault-interval 1 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
+# python main.py --program graph_coloring --sched 0 --no-sim 500000 --fault-interval 2 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
+python main.py --program graph_coloring --sched 0 --no-sim 1000000 --fault-interval 2 --graph-names random_graph_diff_degree_2 --fault-prob 1.0
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 5 --graph-names graph_7 --fault-prob 1.0
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 6 --graph-names graph_7 --fault-prob 1.0
 # python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 5 --graph-names graph_powerlaw_cluster_graph_n30 --fault-prob 1.0
