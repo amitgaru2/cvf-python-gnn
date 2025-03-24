81eadfaa4dd4f28af9a441616a10a9fdd62a98f6
Amit Garu <agaru@uwyo.edu>
2025-03-23 20:10:52 -0600

    updates

--- gnn/v2/helpers_wo_embedding.py
+++ gnn/v2/helpers_wo_embedding.py
@@ -1,6 +1,7 @@
 import os
 import ast
 import json
+import pickle
 
 import torch
 import pandas as pd
@@ -9,22 +10,7 @@ from torch.utils.data import Dataset
 
 
 class CVFConfigDataset(Dataset):
-    embedding_map = torch.tensor(
-        [
-            [-0.5290, -0.0216, -0.0945, 0.2270],
-            [-0.4443, -0.0111, -0.1742, -0.2307],
-            [-0.3748, -0.0128, -0.2559, -0.1789],
-            [-0.5496, -0.0217, -0.1014, 0.2230],
-            [-0.4470, -0.0077, 0.0267, -0.1808],
-            [-0.5436, -0.0245, -0.3175, 0.1133],
-            [-0.4278, -0.0222, -0.3070, 0.1349],
-            [-0.4484, -0.0081, 0.0516, -0.1719],
-            [-0.4758, -0.0172, -0.1159, 0.0017],
-            [-0.4668, -0.0147, 0.0071, 0.0639],
-            [-0.5965, -0.0140, 0.0095, 0.0161],
-            [-0.5079, -0.0156, -0.1128, 0.0029],
-        ]
-    )
+    embedding_map = torch.FloatTensor(pickle.load(open("matrix.dump", "rb")))
 
     def __init__(self, program, dataset_file, A_file, num_nodes) -> None:
         dataset_dir = os.path.join(
@@ -49,7 +35,6 @@ class CVFConfigDataset(Dataset):
         #     ),
         #     torch.tensor([[row["M"]]]).float(),
         # )
-
         result = (
             self.embedding_map[row["config"]].unsqueeze(0),
             torch.FloatTensor([[row["M"]]]),
@@ -61,9 +46,9 @@ class CVFConfigDataset(Dataset):
 if __name__ == "__main__":
     dataset = CVFConfigDataset(
         "coloring",
-        "tiny_graph_test_config_rank_dataset.csv",
-        "tiny_graph_test_A.json",
-        3,
+        "graph_random_regular_graph_n6_d3_config_rank_dataset.csv",
+        "graph_random_regular_graph_n6_d3_A.json",
+        6,
     )
 
     print(dataset[0], dataset[0][0].shape)
