0f42e71775d62769086fa0d275393e5f10fbbb9b
Amit Garu <agaru@uwyo.edu>
2025-04-07 17:06:05 -0600

    updates

--- run_nohup.sh
+++ run_nohup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 export PYTHONDONTWRITEBYTECODE=1
-jobID=$(shuf -i 10000-99999 -n 1)
+dateTime=$(date +"%Y-%m-%d__%H:%M:%S")
+jobID=$(shuf -i 10000-99999 -n 1)__$dateTime
 echo "Job ID: ""$jobID"
 nohup ./nohup_commands.sh > cvf_"$jobID".log 2>&1 <&- &
