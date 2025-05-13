9d0192b0c7ebdf622ebb557e2153127c51bdf5c9
agaru <agaru@disclab1.arcc.uwyo.edu>
2025-05-13 11:47:54 -0600

    updates

--- run_nohup.sh
+++ run_nohup.sh
@@ -4,6 +4,7 @@ export PYTHONDONTWRITEBYTECODE=1
 dateTime=$(date +"%Y-%m-%d__%H:%M:%S")
 jobID=$(shuf -i 10000-99999 -n 1)__$dateTime
 #echo "Job ID: ""$jobID"
-logLocation="nohup_logs/cvf_""$jobID"".log"
+hostName=$(hostname)
+logLocation="nohup_logs/cvf_""$jobID""_""$hostName"".log"
 echo "Log location: ""$logLocation"
 nohup ./nohup_commands.sh > "$logLocation" 2>&1 <&- &
