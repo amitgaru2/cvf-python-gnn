ead7411e9def2cf2ba6b8cfb6f89e55ed8197216
Amit Garu <agaru@uwyo.edu>
2025-05-14 19:20:21 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -75,7 +75,7 @@ graphs=("star_graph_n7" "graph_powerlaw_cluster_graph_n7")
 # graphs=("implicit_graph_n5" "implicit_graph_n6" "implicit_graph_n7" "implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10")
 joined_graphs_args="${graphs[@]}"
 
-epochs=20
+epochs=10
 batch_size=64
 hidden_size=32
 
