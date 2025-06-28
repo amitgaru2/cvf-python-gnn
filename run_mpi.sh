35c522acc9c1ad24cd9aeefc5a3a6eb7faadd509
Amit Garu <agaru@uwyo.edu>
2025-06-28 14:17:53 -0600

    updates

--- run_mpi.sh
+++ run_mpi.sh
@@ -11,4 +11,5 @@ cd gnn
 
 # python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n7_d4
 
-exec python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n7
+# exec python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n7
+exec python "$@" >log_rank_${RANK}.out 2>&1
