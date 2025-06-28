d8cd3c1786cebdd37ec2d783059423c17c2eff81
Amit Garu <agaru@uwyo.edu>
2025-06-28 13:52:07 -0600

    updates

--- run_mpi.sh
+++ run_mpi.sh
@@ -5,8 +5,10 @@ echo "Shell: "$SHELL
 
 source ~/anaconda3/etc/profile.d/conda.sh
 
-conda activate cvf 
+conda activate cvf
 
 cd gnn
 
-python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n7_d4
+# python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n7_d4
+
+python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n8
