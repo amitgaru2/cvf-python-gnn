29a33be025e12c55d90486bf7bb0073c0c63588c
Amit Garu <agaru@uwyo.edu>
2025-07-02 14:28:37 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -7,10 +7,11 @@ set -ex
 # python main.py --program maximal_matching --graph-names graph_10 -ml
 
 cd simulations
-# python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 2 --graph-names graph_10 --fault-prob 1.0 --simulation-type random
-# python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 1 --graph-names star_graph_0_n7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 0
-python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 1 --graph-names star_graph_0_n7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 2
-python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 1 --graph-names star_graph_0_n7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 5
+python simulate.py --program maximal_matching --sched 0 --no-sim 10000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0 --simulation-type random
+python simulate.py --program maximal_matching --sched 0 --no-sim 10000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 0
+python simulate.py --program maximal_matching --sched 0 --no-sim 10000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 4
+python simulate.py --program maximal_matching --sched 0 --no-sim 10000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 5
+python simulate.py --program maximal_matching --sched 0 --no-sim 10000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0 --simulation-type random_start_at_node --controlled-at-node 9
 # python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 2 --graph-names graph_10 --fault-prob 1.0 --simulation-type controlled_at_node_duong --controlled-at-node 0
 # python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 2 --graph-names graph_10 --fault-prob 1.0 --simulation-type controlled_at_node_duong --controlled-at-node 4
 # python simulate.py --program graph_coloring --sched 0 --no-sim 10000 --fault-interval 2 --graph-names graph_10 --fault-prob 1.0 --simulation-type controlled_at_node_duong --controlled-at-node 7
