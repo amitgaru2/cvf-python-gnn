8b1f48b2f88856b28c7afb7c74b7a49cf9e53af5
Amit Garu <agaru@uwyo.edu>
2025-07-09 13:11:20 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -9,13 +9,13 @@ set -ex
 cd simulations
 
 # PROGRAM="graph_coloring"
-PROGRAM="dijkstra_token_ring"
-# PROGRAM="maximal_matching"
+# PROGRAM="dijkstra_token_ring"
+PROGRAM="maximal_matching"
 
-# GRAPH="graph_10"
-GRAPH="implicit_graph_n10"
+GRAPH="graph_7"
+# GRAPH="implicit_graph_n10"
 
-NO_SIMS=100000
+NO_SIMS=10000
 LIMIT_STEPS=100
 FAULT_INTERVALS=(2 3 4 5)
 SIMULATION_TYPE="controlled_at_node_amit_v2"
@@ -26,14 +26,14 @@ for FI in "${FAULT_INTERVALS[@]}"; do
     python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FI --graph-names $GRAPH --fault-prob 1.0 --simulation-type $SIMULATION_TYPE --controlled-at-node 9 --limit-steps $LIMIT_STEPS
 done
 
-# GRAPH="graph_10"
+GRAPH="graph_10"
 
-# for simType in "${SIMULATION_TYPES[@]}"; do
-#     python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type $simType --controlled-at-node 0 --limit-steps $LIMIT_STEPS
-#     python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type $simType --controlled-at-node 4 --limit-steps $LIMIT_STEPS
-#     python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type $simType --controlled-at-node 7 --limit-steps $LIMIT_STEPS
-#     python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type $simType --controlled-at-node 9 --limit-steps $LIMIT_STEPS
-# done
+for FI in "${FAULT_INTERVALS[@]}"; do
+    python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FI --graph-names $GRAPH --fault-prob 1.0 --simulation-type $SIMULATION_TYPE --controlled-at-node 0 --limit-steps $LIMIT_STEPS
+    python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FI --graph-names $GRAPH --fault-prob 1.0 --simulation-type $SIMULATION_TYPE --controlled-at-node 1 --limit-steps $LIMIT_STEPS
+    python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FI --graph-names $GRAPH --fault-prob 1.0 --simulation-type $SIMULATION_TYPE --controlled-at-node 9 --limit-steps $LIMIT_STEPS
+done
+
 
 # SIMULATION_TYPE="controlled_at_node_amit_v2"
 
