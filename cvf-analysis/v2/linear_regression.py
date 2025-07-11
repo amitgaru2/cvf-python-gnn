a0b7658d87d95127345e47773117627c2f22cd01
Amit Garu <agaru@uwyo.edu>
2025-07-11 12:43:17 -0600

    updates

--- cvf-analysis/v2/linear_regression.py
+++ cvf-analysis/v2/linear_regression.py
@@ -83,7 +83,7 @@ class LinearRegressionCVFAnalysisV2(CVFAnalysisV2):
     def __get_node_data_df(self, node_id):
         return self.lr_config.df[self.config.df["node"] == node_id]
 
-    def __clean_float_to_step_size_single(self, slope):
+    def __clean_to_step_size(self, slope):
         quotient = np.divide(slope, self.config.slope_step)
         if quotient == int(quotient):
             return np.round(slope, self.config.slope_step_decimals)
@@ -91,52 +91,62 @@ class LinearRegressionCVFAnalysisV2(CVFAnalysisV2):
             np.int64(quotient) * self.config.slope_step, self.config.slope_step_decimals
         )
 
-    def __copy_replace_indx_value(self, lst, indx, value):
-        lst_copy = lst.copy()
-        lst_copy[indx] = value
-        return lst_copy
+    # def __copy_replace_indx_value(self, lst, indx, value):
+    #     lst_copy = lst.copy()
+    #     lst_copy[indx] = value
+    #     return lst_copy
 
     def _get_program_transitions_as_configs(self, start_state):
-        node_params = list(start_state)
 
-        for node_id in range(self.nodes):
+        for i in range(len(self.nodes)):
+            data = self.get_actual_config_node_values(i, start_state[i])
+
             for _ in range(1, self.config.iterations + 1):
-                m = torch.tensor(node_params[node_id], requires_grad=True)
-                c = torch.tensor(node_params[node_id], requires_grad=True)
+                m = torch.tensor(data.m, requires_grad=True)
+                c = torch.tensor(data.c, requires_grad=True)
 
-                start_state_cpy = list(start_state)
-                start_state_cpy[node_id] = m.item()
-
-                node_df = self.__get_node_data_df(node_id)
+                node_df = self.__get_node_data_df(i)
                 X_node = torch.tensor(node_df["X"].array)
-                y = m * X_node
+                y = m * X_node + c
                 y.backward()
 
-                doubly_st_mt = self.doubly_stochastic_matrix_config[node_id]
-
-                new_slope = (
-                    sum(
-                        frac * start_state_cpy[j] for j, frac in enumerate(doubly_st_mt)
-                    )
+                # new values to update
+                doubly_st_mt = self.lr_config.config.doubly_stochastic_matrix[i]
+                new_m = (
+                    sum(wt * data.m for wt in doubly_st_mt)
                     - self.learning_rate * m.grad
                 )
-
-                if new_slope > self.config.max_slope:
-                    new_slope = self.config.max_slope
-
-                node_params[node_id] = new_slope
-
-                if abs(m - new_slope) <= self.config.iteration_stop_threshold:
-                    break
-
-        for node_id, new_slope in enumerate(node_params):
-            new_slope_cleaned = self.__clean_float_to_step_size_single(new_slope)
-            if new_slope_cleaned != start_state[node_id]:
-                new_node_params = self.__copy_replace_indx_value(
-                    list(start_state), node_id, new_slope_cleaned
+                new_c = (
+                    sum(wt * data.c for wt in doubly_st_mt)
+                    - self.learning_rate * c.grad
                 )
-                new_node_params = tuple(new_node_params)
-                yield node_id, new_node_params
+
+                # start_state_cpy = list(start_state)
+                # start_state_cpy[node_id] = m.item()
+
+                # new_slope = (
+                #     sum(
+                #         frac * start_state_cpy[j] for j, frac in enumerate(doubly_st_mt)
+                #     )
+                #     - self.learning_rate * m.grad
+                # )
+
+                # if new_slope > self.config.max_slope:
+                #     new_slope = self.config.max_slope
+
+                # node_params[node_id] = new_slope
+
+                # if abs(m - new_slope) <= self.config.iteration_stop_threshold:
+                #     break
+
+        # for node_id, new_slope in enumerate(node_params):
+        #     new_slope_cleaned = self.__clean_float_to_step_size_single(new_slope)
+        #     if new_slope_cleaned != start_state[node_id]:
+        #         new_node_params = self.__copy_replace_indx_value(
+        #             list(start_state), node_id, new_slope_cleaned
+        #         )
+        #         new_node_params = tuple(new_node_params)
+        #         yield node_id, new_node_params
 
 
 if __name__ == "__main__":
