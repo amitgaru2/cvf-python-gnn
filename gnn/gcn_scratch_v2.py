e1557caf86375ef67420e217e0712031bcb0ef46
Amit Garu <agaru@uwyo.edu>
2025-07-31 10:13:16 -0600

    updates

--- gnn/gcn_scratch_v2.py
+++ gnn/gcn_scratch_v2.py
@@ -63,35 +63,6 @@ class SimpleGCN(nn.Module):
             )
 
 
-# class CustomBatchSampler(Sampler):
-#     def __init__(self, datasets: ConcatDataset, batch_size: int):
-#         self.datasets = datasets
-#         self.batch_size = batch_size
-
-#     def __iter__(self):
-#         last_accessed = [0] + self.datasets.cumulative_sizes[:]
-#         end_loop = [False for _ in range(len(self.datasets.datasets))]
-
-#         while not all(end_loop):
-#             for turn in range(len(self.datasets.datasets)):
-#                 if end_loop[turn]:
-#                     continue
-
-#                 batch_size = self.batch_size
-#                 if (
-#                     last_accessed[turn] + batch_size
-#                     >= self.datasets.cumulative_sizes[turn]
-#                 ):
-#                     batch_size = (
-#                         self.datasets.cumulative_sizes[turn] - last_accessed[turn]
-#                     )
-#                     end_loop[turn] = True
-
-#                 yield list(range(last_accessed[turn], last_accessed[turn] + batch_size))
-
-#                 last_accessed[turn] += batch_size
-
-
 def get_dataset_coll(program, *graph_names):
     dataset_coll = []
 
@@ -108,55 +79,51 @@ def get_dataset_coll(program, *graph_names):
     return dataset_coll
 
 
-# def test_model(model, test_concat_datasets, save_result=False):
-#     if save_result:
-#         f = open(
-#             f"test_results/test_result_w_succ_diff_nodes_gcn_script_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}.csv",
-#             "w",
-#             newline="",
-#         )
-#         csv_writer = csv.writer(f)
-#         csv_writer.writerow(["Dataset", "Actual", "Predicted"])
+def test_model(model, test_concat_datasets, save_result=False):
+    if save_result:
+        f = open(
+            f"test_results/test_result_w_succ_diff_nodes_gcn_script_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}.csv",
+            "w",
+            newline="",
+        )
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["Dataset", "Actual", "Predicted"])
 
-#     criterion = torch.nn.MSELoss()
+    criterion = torch.nn.MSELoss()
 
-#     model.eval()
+    model.eval()
 
-#     with torch.no_grad():
-#         # test_concat_datasets = ConcatDataset(test_datasets)
-#         test_batch_sampler = CustomBatchSampler(test_concat_datasets, batch_size=10240)
-#         test_dataloader = DataLoader(
-#             test_concat_datasets, batch_sampler=test_batch_sampler
-#         )
+    with torch.no_grad():
+        test_dataloader = DataLoader(test_concat_datasets)
 
-#         total_loss = 0
-#         total_matched = 0
-#         count = 0
-#         for batch in test_dataloader:
-#             x = batch[0]
-#             y = batch[1]
-#             y = y.unsqueeze(-1)
-#             out = model(x[0], x[1])
-#             if save_result:
-#                 csv_writer.writerows(
-#                     (i, j.item(), k.item())
-#                     for (i, j, k) in zip(
-#                         x[1], y.detach().cpu().numpy(), out.detach().cpu().numpy()
-#                     )
-#                 )
-#             loss = criterion(out, y)
-#             total_loss += loss
-#             out = torch.round(out)
-#             matched = (out == y).sum().item()
-#             total_matched += matched
-#             count += 1
+        total_loss = 0
+        total_matched = 0
+        count = 0
+        for batch in test_dataloader:
+            x = batch[0]
+            y = batch[1]
+            y = y.unsqueeze(-1)
+            out = model(x[0], x[1][0])
+            if save_result:
+                csv_writer.writerows(
+                    (i, j.item(), k.item())
+                    for (i, j, k) in zip(
+                        x[1], y.detach().cpu().numpy(), out.detach().cpu().numpy()
+                    )
+                )
+            loss = criterion(out, y)
+            total_loss += loss
+            out = torch.round(out)
+            matched = (out == y).sum().item()
+            total_matched += matched
+            count += 1
 
-#         logger.info(
-#             f"Test set | MSE loss: {round((total_loss / count).item(), 4)} | Total matched: {total_matched:,} out of {len(test_concat_datasets):,} (Accuracy: {round(total_matched / len(test_concat_datasets) * 100, 2):,}%)",
-#         )
+        logger.info(
+            f"Test set | MSE loss: {round((total_loss / count).item(), 4)} | Total matched: {total_matched:,} out of {len(test_concat_datasets):,} (Accuracy: {round(total_matched / len(test_concat_datasets) * 100, 2):,}%)",
+        )
 
-#     if save_result:
-#         f.close()
+    if save_result:
+        f.close()
 
 
 def main(program, graph_names, H, batch_size, epochs):
@@ -208,9 +175,9 @@ def main(program, graph_names, H, batch_size, epochs):
     model_name = f"trained_models/gcn_trained_at_{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')}.pt"
     logger.info("Saving model %s", model_name)
     torch.save(model, model_name)
-    # logger.info("\n")
-    # logger.info("Testing model...")
-    # test_model(model, test_concat_datasets, save_result=True)
+    logger.info("\n")
+    logger.info("Testing model...")
+    test_model(model, test_concat_datasets, save_result=True)
 
 
 if __name__ == "__main__":
