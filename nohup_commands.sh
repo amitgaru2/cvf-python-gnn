75e4361f5747ea1fb0aaddd2b1ca97974863775c
Amit Garu <agaru@uwyo.edu>
2025-08-29 12:38:38 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -98,20 +98,20 @@ MAX_MATCHING_PROGRAM="maximal_matching"
 
 cd gnn
 
-graphs=("star_graph_n8" "star_graph_n9" "star_graph_n10" "star_graph_n11" \
-        "graph_powerlaw_cluster_graph_n5" "graph_powerlaw_cluster_graph_n6" "graph_powerlaw_cluster_graph_n7" "graph_powerlaw_cluster_graph_n8" \
-        "graph_random_regular_graph_n7_d2" "graph_random_regular_graph_n7_d4" "graph_random_regular_graph_n8_d2" "graph_random_regular_graph_n8_d4")
+# graphs=("star_graph_n8" "star_graph_n9" "star_graph_n10" "star_graph_n11" \
+#         "graph_powerlaw_cluster_graph_n5" "graph_powerlaw_cluster_graph_n6" "graph_powerlaw_cluster_graph_n7" "graph_powerlaw_cluster_graph_n8" \
+#         "graph_random_regular_graph_n7_d2" "graph_random_regular_graph_n7_d4" "graph_random_regular_graph_n8_d2" "graph_random_regular_graph_n8_d4")
 
-# graphs=("implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10" "implicit_graph_n11")
+graphs=("implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10" "implicit_graph_n11")
 
 joined_graphs_args="${graphs[@]}"
 
-epochs=25
+epochs=50
 batch_size=64
 hidden_size=32
 
 python lstm_scratch.py \
-    --program $GRAPH_COLORING_PROGRAM \
+    --program $DIJKSTRA_TOKEN_PROGRAM \
     --epochs $epochs \
     --batch-size $batch_size \
     --hidden-size $hidden_size \
