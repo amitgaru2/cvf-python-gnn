06e7f5c5f32ac2a389bc82f9d964485b671990b8
Amit Garu <agaru@uwyo.edu>
2025-08-29 12:36:59 -0600

    updates

--- nohup_commands.sh
+++ nohup_commands.sh
@@ -6,17 +6,17 @@ DIJKSTRA_TOKEN_PROGRAM="dijkstra_token_ring"
 MAX_MATCHING_PROGRAM="maximal_matching"
 
 
-cd cvf-analysis
+# cd cvf-analysis
 
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names star_graph_n10 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n10_d4 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n11_d4 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n12_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names star_graph_n10 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n10_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n11_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n12_d4 -ml
 
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n9_d4 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n10_d4 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n11_d4 -ml
-python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n12_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n9_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n10_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n11_d4 -ml
+# python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regular_graph_n12_d4 -ml
 
 
 # cd simulations
@@ -96,66 +96,28 @@ python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regu
 # python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type controlled_at_node_duong --controlled-at-node 8 --limit-steps $LIMIT_STEPS
 # python simulate.py --program $PROGRAM --sched 0 --no-sim $NO_SIMS --fault-interval $FAULT_INTERVAL --graph-names $GRAPH --fault-prob 1.0 --simulation-type controlled_at_node_duong --controlled-at-node 9 --limit-steps $LIMIT_STEPS
 
-# cd gnn
-# python cvf_analysis.py lstm_trained_at_2025_05_12_21_31 dijkstra implicit_graph_n14
-# python cvf_analysis.py lstm_trained_at_2025_04_26_14_01 coloring graph_random_regular_graph_n10_d3
-# python cvf_analysis.py lstm_trained_at_2025_05_15_11_29 $PROGRAM star_graph_n7
-# python cvf_analysis.py lstm_trained_at_2025_05_15_11_29 $PROGRAM graph_powerlaw_cluster_graph_n7
-# python cvf_analysis.py lstm_trained_at_2025_05_15_11_29 maximal_matching graph_random_regular_graph_n7_d4
+cd gnn
 
-# # # # graphs=("star_graph_n7" "graph_powerlaw_cluster_graph_n7" "graph_random_regular_graph_n7_d4" "star_graph_n13" "graph_powerlaw_cluster_graph_n8" "graph_random_regular_graph_n8_d4" "star_graph_n15" "graph_powerlaw_cluster_graph_n9")
-# graphs=("star_graph_n7" "graph_powerlaw_cluster_graph_n7")
-# # graphs=("star_graph_n7")
-# # graphs=("implicit_graph_n5" "implicit_graph_n6" "implicit_graph_n7" "implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10")
-# joined_graphs_args="${graphs[@]}"
+graphs=("star_graph_n8" "star_graph_n9" "star_graph_n10" "star_graph_n11" \
+        "graph_powerlaw_cluster_graph_n5" "graph_powerlaw_cluster_graph_n6" "graph_powerlaw_cluster_graph_n7" "graph_powerlaw_cluster_graph_n8" \
+        "graph_random_regular_graph_n7_d2" "graph_random_regular_graph_n7_d4" "graph_random_regular_graph_n8_d2" "graph_random_regular_graph_n8_d4")
 
-# epochs=10
-# batch_size=1024
-# hidden_size=32
+# graphs=("implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10" "implicit_graph_n11")
 
-# python main.py --program graph_coloring --sched 0 --no-sim 100000 --fault-interval 1 --graph-names graph_7 --fault-prob 1.0
-# python main.py --program dijkstra_token_ring --sched 0 --no-sim 500000 --fault-interval 1 --graph-names implicit_graph_n5 --fault-prob 1.0
-# python main.py --program dijkstra_token_ring --sched 0 --no-sim 500000 --fault-interval 2 --graph-names implicit_graph_n5 --fault-prob 1.0
-# python main.py --program dijkstra_token_ring --sched 0 --no-sim 500000 --fault-interval 4 --graph-names implicit_graph_n5 --fault-prob 1.0
+joined_graphs_args="${graphs[@]}"
 
-# cd gnn
+epochs=25
+batch_size=64
+hidden_size=32
 
-# # # # # graphs=("star_graph_n7" "graph_powerlaw_cluster_graph_n7" "graph_random_regular_graph_n7_d4" "star_graph_n13" "graph_powerlaw_cluster_graph_n8" "graph_random_regular_graph_n8_d4" "star_graph_n15" "graph_powerlaw_cluster_graph_n9")
-# # # # # graphs=("graph_powerlaw_cluster_graph_n6" "graph_random_regular_graph_n6_d3")
-# # # # # # # # # graphs=("implicit_graph_n5" "implicit_graph_n6" "implicit_graph_n7" "implicit_graph_n8" "implicit_graph_n9" "implicit_graph_n10")
+python lstm_scratch.py \
+    --program $GRAPH_COLORING_PROGRAM \
+    --epochs $epochs \
+    --batch-size $batch_size \
+    --hidden-size $hidden_size \
+    --num-layers 2 \
+    --graph-names $joined_graphs_args
 
-# graphs=("star_graph_n7")
-# graphs=("star_graph_n15")
-
-# graphs=("graph_powerlaw_cluster_graph_n8")
-
-# graphs=("graph_random_regular_graph_n8_d4")
-
-# graphs=("implicit_graph_n10")
-# graphs=("implicit_graph_n11")
-# graphs=("implicit_graph_n12")
-
-# graphs=("graph_powerlaw_cluster_graph_n6")
-# graphs=("graph_random_regular_graph_n6_d2")
-
-
-
-# # joined_graphs_args="${graphs[@]}"
-
-# epochs=25
-# batch_size=64
-# hidden_size=32
-
-# python lstm_scratch.py \
-#     --program $DIJKSTRA_TOKEN_PROGRAM \
-#     --epochs $epochs \
-#     --batch-size $batch_size \
-#     --hidden-size $hidden_size \
-#     --num-layers 2 \
-#     --graph-names $joined_graphs_args
-
-
-# graphs=("star_graph_n8" "graph_powerlaw_cluster_graph_n6" "graph_random_regular_graph_n6_d3")
 
 # for joined_graphs_args in "${graphs[@]}"; do
 
@@ -167,33 +129,3 @@ python main.py --program $GRAPH_COLORING_PROGRAM --graph-names graph_random_regu
 #         --graph-names $joined_graphs_args
 
 # done
-
-# # python gcn_scratch.py \
-# #     --epochs $epochs \
-# #     --batch-size $batch_size \
-# #     --hidden-size $hidden_size \
-# #     --graph-names $joined_graphs_args
-
-# # cd gnn/new_ideas
-# #
-# # python bert_scratch.py
-# # python transformer_w_same_node_seql.py 50
-
-# cd gnn
-
-# python cvf_analysis.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n4_d2
-# python cvf_analysis.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n5_d4
-# python cvf_analysis.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n6_d3
-# python cvf_analysis.py --model lstm_trained_at_2025_06_25_19_06 --program maximal_matching --graph-names graph_powerlaw_cluster_graph_n7
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n4
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n5
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n6
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n7
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n8
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_17_35 --program maximal_matching --graph-names star_graph_n9
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_13_36 --program maximal_matching --graph-names graph_powerlaw_cluster_graph_n5 graph_powerlaw_cluster_graph_n6
-# python cvf_analysis.py --model lstm_trained_at_2025_06_27_13_36 --program maximal_matching --graph-names graph_powerlaw_cluster_graph_n5 graph_powerlaw_cluster_graph_n6
-# mpirun -n 4 python cvf_analysis_mpi.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n7_d4
-
-# python cvf_analysis.py --model lstm_trained_at_2025_04_26_14_01 --program coloring --graph-names graph_10
-# mpirun --hostfile hosts.txt ./run_mpi.sh cvf_analysis_mpi.py --model lstm_trained_at_2025_06_26_20_32 --program maximal_matching --graph-names graph_random_regular_graph_n8_d4
