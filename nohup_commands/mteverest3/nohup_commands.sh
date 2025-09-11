bfb6c8ce2d10942a68b6871787c6dda49a6b4948
Amit Garu <agaru@uwyo.edu>
2025-09-11 07:25:40 -0600

    updates

--- nohup_commands/mteverest3/nohup_commands.sh
+++ nohup_commands/mteverest3/nohup_commands.sh
@@ -10,4 +10,9 @@ GC_MODEL="lstm_trained_at_2025_08_29_23_08"
 DTR_MODEL="lstm_trained_at_2025_08_29_23_28"
 MM_MODEL="lstm_trained_at_2025_08_30_05_53"
 
-echo $GC_MODEL
+CVF_ANALYSISI_DIR="cvf_analysis"
+GNN_DIR="gnn"
+
+cd $GNN_DIR
+
+python cvf_analysis.py --model $MM_MODEL --program $MM_PROG --graph-names graph_random_regular_graph_n9_d2
