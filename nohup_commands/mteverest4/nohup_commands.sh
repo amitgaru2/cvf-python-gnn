21099a83033f94d8ffa15eb0faccaa465d8df080
Amit Garu <agaru@uwyo.edu>
2025-09-11 09:51:21 -0600

    updates

--- nohup_commands/mteverest4/nohup_commands.sh
+++ nohup_commands/mteverest4/nohup_commands.sh
@@ -10,4 +10,5 @@ GC_MODEL="lstm_trained_at_2025_08_29_23_08"
 DTR_MODEL="lstm_trained_at_2025_08_29_23_28"
 MM_MODEL="lstm_trained_at_2025_08_30_05_53"
 
-echo $GC_MODEL
+cd gnn
+python cvf_analysis.py --model $MM_MODEL --program maximal_matching --graph-names star_graph_n11
