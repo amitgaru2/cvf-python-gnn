be1a4a76a2502615005a162ab16ccdd8bb541485
Amit Garu <agaru@uwyo.edu>
2025-10-06 16:17:06 -0600

    updates

--- riak_client/graph_coloring.py
+++ riak_client/graph_coloring.py
@@ -3,8 +3,6 @@ import sys
 
 import requests
 
-from chash_vnode_index import get_vnode_index
-
 utils_path = os.path.join(os.getenv("CVF_PROJECT_DIR", ""), "utils")
 sys.path.append(utils_path)
 
@@ -17,6 +15,7 @@ RING_SIZE = 8
 RIAK_BASE_URL = "http://localhost:8098"
 RIAK_BUCKET_PREFIX = "graph_coloring"
 RIAK_NODE_KEY_PREFIX = "node_"
+RIAK_PETERSON_LCK_KEY_PREFIX = "L_"
 
 
 def put_request_riak(bucket_name, key, value):
@@ -34,7 +33,7 @@ def put_request_riak(bucket_name, key, value):
     try:
         response = requests.put(url, json=data, headers=headers)
         response.raise_for_status()
-        logger.info(f"Wrote {{ {key}: {value} }} to {bucket_name}.")
+        logger.info(f"Wrote {{ {key}: {value} }} to the Bucket: {bucket_name}.")
         logger.debug(f"Success: {response.status_code}")
         if response.text:
             logger.debug("Response body:", response.text)
@@ -47,6 +46,17 @@ def put_request_riak(bucket_name, key, value):
     except Exception as e:
         logger.error(f"Error: {e}")
         return False
+
+
+def init_pet_lock_data(riak_bucket_name, graph):
+    logger.info(f"Writing initial Peterson lock data.")
+
+    for n in graph.nodes():
+        for nbr in graph.neighbors(n):
+            if n < nbr:
+                node_key = f"{RIAK_PETERSON_LCK_KEY_PREFIX}{n}_{nbr}"
+                meta = {"flag_0": False, "flag_1": False, "turn": None}
+                put_request_riak(riak_bucket_name, node_key, meta)
 
 
 def init_graph_data(riak_bucket_name, graph):
@@ -73,6 +83,7 @@ def init_config_data(riak_bucket_name, graph):
 def init_data(riak_bucket_name, graph):
     init_graph_data(riak_bucket_name, graph)
     init_config_data(riak_bucket_name, graph)
+    init_pet_lock_data(riak_bucket_name, graph)
 
 
 def invert_dict(d):
