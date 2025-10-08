9958ff7cc84c00653669d6b76ab87ea8680da581
Amit Garu <agaru@uwyo.edu>
2025-10-08 11:15:25 -0600

    updates

---
+++ riak_client/init_experiment.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ===================================================
+# This script is meant to be run in the local machine
+# ===================================================
+set -eu
+
+# variables defined to be changed
+SERVER_MACHINES=("manaslu5.uwyo.edu" "manaslu6.uwyo.edu" "manaslu7.uwyo.edu")
+CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu")
+
+CLIENT_COPY_FILES=("client_helpers.py" "graph_coloring.py" "run_nohup.sh" "nohup_commands.sh")
+# end of variables to be changed
+
+# copy the client script to all client machines
+for client in "${CLIENT_MACHINES[@]}"; do
+    echo "Setting up client machine: $client."
+    ssh "$client" "rm -rf ~/research/client_scripts"
+    ssh "$client" "mkdir -p ~/research/client_scripts"
+    echo "Copying run_nohup.sh to $client."
+    for file in "${CLIENT_COPY_FILES[@]}"; do
+        scp "$file" "$client:~/research/client_scripts/"
+    done
+    echo -e "Done copying files to $client.\n"
+done
+
+# execute the client script on all client machines
+for client_id in "${!CLIENT_MACHINES[@]}"; do
+    client="${CLIENT_MACHINES[$client_id]}"
+    echo "Executing run_nohup.sh on $client."
+    # ssh "$client" "cd ~/research/client_scripts && bash run_nohup.sh"
+    echo -e "Done executing run_nohup.sh on $client.\n"
+done
+
+echo "=== Experiment initialization complete. ==="
