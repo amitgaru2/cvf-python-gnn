e35639e069a1ab376850272ad7786024dc359600
Amit Garu <agaru@uwyo.edu>
2025-10-09 15:56:53 -0600

    updates

---
+++ riak_client/erlang_imp/prepare_riak.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env escript
+%%! -smp enable -sname prepare_riak
+
+main(Args) ->
+    %% Call your logger setup
+    %% my_logger:setup(),
+
+    %% Parse arguments (simple example)
+    case Args of
+        ["--graph-name", GraphName] ->
+            io:format("Graph name: ~s~n", [GraphName]),
+            %% Here you would call your Riak initialization functions
+            ok;
+        _ ->
+            io:format("Usage: ./prepare_riak --graph-name <name>~n"),
+            halt(1)
+    end.
