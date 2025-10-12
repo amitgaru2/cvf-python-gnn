3c70fa5e58f4f003a87ec8240dcc062e7174b80a
Amit Garu <agaru@uwyo.edu>
2025-10-12 16:16:58 -0600

    updates

--- riak_client/collect_client_logs.sh
+++ riak_client/collect_client_logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -eu
 # ===================================================
-CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
+CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "yangra4.uwyo.edu" \
+                 "yangra5.uwyo.edu" "yangra6.uwyo.edu" "yangra7.uwyo.edu" "yangra8.uwyo.edu" \
+                 "yangra9.uwyo.edu" "yangra10.uwyo.edu" "yangra11.uwyo.edu" \
+                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
+                 "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
 dateDir="25_10_12"
-jobID="06_43_80143"
+jobID="14_34_69925"
 # ===================================================
 # collect the client logs from all client machines
 for client in "${CLIENT_MACHINES[@]}"; do
