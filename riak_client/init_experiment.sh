3278d23edd7444093b85df659d61b95663f4ef24
Amit Garu <agaru@uwyo.edu>
2025-10-12 13:21:48 -0600

    updates

--- riak_client/init_experiment.sh
+++ riak_client/init_experiment.sh
@@ -8,7 +8,11 @@ set -eu
 GRAPH_NAME="$1"
 
 SERVER_MACHINES=("manaslu5.uwyo.edu:8098" "manaslu6.uwyo.edu:8098" "manaslu7.uwyo.edu:8098" "manaslu8.uwyo.edu:8098")
-CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
+CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "yangra4.uwyo.edu" \
+                 "yangra5.uwyo.edu" "yangra6.uwyo.edu" "yangra7.uwyo.edu" "yangra8.uwyo.edu" \
+                 "yangra9.uwyo.edu" "yangra10.uwyo.edu" "yangra11.uwyo.edu" \
+                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
+                 "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
 
 
 CLIENT_COPY_FILES=("run_nohup.sh" "nohup_commands.sh" "custom_logger.py" "riak_helpers.py" "client_helpers.py" "graph_helpers.py" "graph_coloring.py" "verify_graph_coloring.py")
