b0505a8c8a48ddf5c28d03fd2c8a5244b5849217
Amit Garu <agaru@uwyo.edu>
2025-10-12 16:30:13 -0600

    updates

--- riak_client/reset_db.sh
+++ riak_client/reset_db.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eu
 
-SERVER_MACHINES=("manaslu5.uwyo.edu:8098" "manaslu6.uwyo.edu:8098" "manaslu7.uwyo.edu:8098" "manaslu8.uwyo.edu:8098")
+SERVER_MACHINES=("manaslu5.uwyo.edu" "manaslu6.uwyo.edu" "manaslu7.uwyo.edu" "manaslu8.uwyo.edu")
 
 # copy the client script to all client machines
 for server in "${SERVER_MACHINES[@]}"; do
     echo "Cleaning up machine: $server."
     ssh "$server" "~/research/riak/_build/rel/rel/riak/bin/riak stop"
     ssh "$server" "rm -rf ~/research/riak/_build/rel/rel/riak/data/bitcask/*"
+    echo "Contents of bitcask after deletion (should be empty):"
+    ssh "$server" "ls ~/research/riak/_build/rel/rel/riak/data/bitcask/*"
     ssh "$server" "~/research/riak/_build/rel/rel/riak/bin/riak daemon"
     echo -e "Done cleaning up machine: $server.\n"
 done
