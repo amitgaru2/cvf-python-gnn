f744f17312de46539b33a08bbb6f53c751401966
Amit Garu <agaru@uwyo.edu>
2025-10-14 16:33:44 -0600

    updates

--- riak_client/collect_client_logs.sh
+++ riak_client/collect_client_logs.sh
@@ -4,11 +4,11 @@ set -eu
 CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "yangra4.uwyo.edu" \
                  "yangra5.uwyo.edu" "yangra6.uwyo.edu" "yangra7.uwyo.edu" "yangra8.uwyo.edu" \
                  "yangra9.uwyo.edu" "yangra10.uwyo.edu" "yangra11.uwyo.edu" \
-                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
                  "nuptse1.uwyo.edu" "nuptse2.uwyo.edu" "nuptse3.uwyo.edu" "nuptse4.uwyo.edu" \
+                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
                  "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
 
-# CLIENT_MACHINES=("nuptse1.uwyo.edu" "nuptse2.uwyo.edu" "nuptse3.uwyo.edu" "nuptse4.uwyo.edu")
+CLIENT_MACHINES=("nuptse1.uwyo.edu")
 dateDir="25_10_14"
 jobID="11_10_22492"
 # ===================================================
--- riak_client/init_experiment.sh
+++ riak_client/init_experiment.sh
@@ -11,8 +11,8 @@ SERVER_MACHINES=("manaslu5.uwyo.edu:8098" "manaslu6.uwyo.edu:8098" "manaslu7.uwy
 CLIENT_MACHINES=("yangra1.uwyo.edu" "yangra2.uwyo.edu" "yangra3.uwyo.edu" "yangra4.uwyo.edu" \
                  "yangra5.uwyo.edu" "yangra6.uwyo.edu" "yangra7.uwyo.edu" "yangra8.uwyo.edu" \
                  "yangra9.uwyo.edu" "yangra10.uwyo.edu" "yangra11.uwyo.edu" \
-                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
                  "nuptse1.uwyo.edu" "nuptse2.uwyo.edu" "nuptse3.uwyo.edu" "nuptse4.uwyo.edu" \
+                 "manaslu3.uwyo.edu" "manaslu4.uwyo.edu" \
                  "manaslu9.uwyo.edu" "manaslu10.uwyo.edu" "manaslu11.uwyo.edu" "manaslu12.uwyo.edu")
 
 
@@ -21,9 +21,9 @@ CLIENT_COPY_FILES=("run_nohup.sh" "nohup_commands.sh" "custom_logger.py" "riak_h
 CLIENT_COPY_DIRS=()
 
 BUCKET__PROPS__N_VAL=3
-BUCKET__PROPS__R=1
-BUCKET__PROPS__W=1
-BUCKET__PROPS__DW=1
+BUCKET__PROPS__R=2
+BUCKET__PROPS__W=2
+BUCKET__PROPS__DW=2
 # end of variables to be changed
 
 # copy the client script to all client machines
