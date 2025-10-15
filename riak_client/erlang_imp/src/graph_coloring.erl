f4b010d2f80f2a833c89b929b7a4924b2b363928
Amit Garu <agaru@uwyo.edu>
2025-10-15 14:57:15 -0600

    updates

--- riak_client/erlang_imp/src/graph_coloring.erl
+++ riak_client/erlang_imp/src/graph_coloring.erl
@@ -70,10 +70,73 @@ get_lexically_ordered_neighbors(Graph, Node) ->
     Neighbors = maps:get(<<"nbrs">>, Response),
     lists:sort(Neighbors).
 
-get_pet_lock(Node, Nbr) ->
+get_i_j_ordering(Node, Nbr) when Node < Nbr ->
+    {Node, Nbr};
+get_i_j_ordering(Node, Nbr) ->
+    {Nbr, Node}.
+
+loop_until(ConditionFun) ->
+    case ConditionFun() of
+        true ->
+            loop_until(ConditionFun);
+        false ->
+            ok
+    end.
+
+get_pet_lock(Graph, Node, Nbr) ->
     Side = Node,
     OtherSide = Nbr,
+    {i, j} = get_i_j_ordering(Node, Nbr),
+    riak_client:put_request_riak(
+        io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+        io_lib:format("L_FLAG_~p_~p_~p", [i, j, Side]),
+        true
+    ),
+    riak_client:put_request_riak(
+        io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+        io_lib:format("L_TURN_~p_~p", [i, j]),
+        Side
+    ),
+    ConditionFun = fun() ->
+        Turn = riak_client:get_request_riak(
+            io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+            io_lib:format("L_TURN_~p_~p", [i, j])
+        ),
+        FlagOtherSide = riak_client:get_request_riak(
+            io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+            io_lib:format("L_FLAG_~p_~p_~p", [i, j, OtherSide])
+        ),
+        (Turn == Side) and (FlagOtherSide == true)
+    end,
+    loop_until(ConditionFun),
     ok.
+
+release_pet_lock(Graph, Node, Nbr) ->
+    Side = Node,
+    {i, j} = get_i_j_ordering(Node, Nbr),
+    riak_client:put_request_riak(
+        io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+        io_lib:format("L_FLAG_~p_~p_~p", [i, j, Side]),
+        false
+    ),
+    ok.
+
+get_neighbor_color([], Graph, Node, Partition, {NbrColors, AcquiredLocks}) ->
+    {NbrColors, AcquiredLocks};
+get_neighbor_color([Nbr | Rest], Graph, Node, Partition, {NbrColors, AcquiredLocks}) ->
+    case lists:member(Nbr, Partition) of
+        true ->
+            NewAcquiredLocks = AcquiredLocks;
+        false ->
+            get_pet_lock(Graph, Node, Nbr),
+            NewAcquiredLocks = [Nbr | AcquiredLocks]
+    end,
+    NbrColor = riak_client:get_request_riak(
+        io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+        io_lib:format("node~p__val", [Nbr])
+    ),
+    NewNbrColors = [NbrColor | NbrColors],
+    get_neighbor_color(Rest, Graph, Node, Partition, {NewNbrColors, NewAcquiredLocks}).
 
 take_step_each_node(Graph, Node, Partition) ->
     SelfColor = riak_client:get_request_riak(
@@ -82,22 +145,24 @@ take_step_each_node(Graph, Node, Partition) ->
     ),
     my_logger:info(io_lib:format("Node ~p has color: ~p~n", [Node, SelfColor])),
     SortedNeighbors = get_lexically_ordered_neighbors(Graph, Node),
-    lists:foreach(
-        fun(Nbr) ->
-            case lists:member(Nbr, Partition) of
-                true ->
-                    ok;
-                false ->
-                    get_pet_lock(Node, Nbr)
-            end,
-            NbrColor = riak_client:get_request_riak(
-                io_lib:format("graph_coloring__~s", [Graph#graph.name]),
-                io_lib:format("node~p__val", [Nbr])
-            ),
-            ok
-        end,
-        SortedNeighbors
+    LockAcquired = [],
+    {NbrColors, AcquiredLocks} = get_neighbor_color(
+        SortedNeighbors, Graph, Node, Partition, LockAcquired
     ),
+    case lists:member(SelfColor, NbrColors) of
+        true ->
+            NewColor = lists:min(lists:seq(0, length(NbrColors)) -- NbrColors),
+            riak_client:put_request_riak(
+                io_lib:format("graph_coloring__~s", [Graph#graph.name]),
+                io_lib:format("node~p__val", [Node]),
+                NewColor
+            ),
+            ok;
+        false ->
+            ok
+    end,
+    % release locks
+    % release_pet_lock(Graph, Node, Nbr) || Nbr <- AcquiredLocks,
     ok.
 
 take_step(Graph, Partition) ->
