9a039a39f51fdc5c386147901e048b6bb1bdd67b
Amit Garu <agaru@uwyo.edu>
2025-10-17 20:15:23 -0600

    updates

--- nohup_commands/mteverest2/nohup_commands.sh
+++ nohup_commands/mteverest2/nohup_commands.sh
@@ -6,3 +6,4 @@ cd riak_client
 # ./init_experiment.sh graph_dblp_coauthorship_n317080
 # ./init_experiment.sh graph_youtube_connection_n1134890
 ./init_experiment.sh graph_youtube_connection_n3223585
+# ./init_experiment.sh complete_graph_n100
