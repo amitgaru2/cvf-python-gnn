861788fd4c07153243d213ce7e57ea7d0001a6d0
Amit Garu <agaru@uwyo.edu>
2025-11-17 15:35:30 -0700

    updates

--- nohup_commands/mteverest2/nohup_commands.sh
+++ nohup_commands/mteverest2/nohup_commands.sh
@@ -10,5 +10,7 @@ cd riak_client
 # ./init_experiment.sh complete_graph_n100
 # ./init_experiment.sh complete_graph_n100
 # ./verify_results.sh complete_graph_n100
-./init_experiment.sh complete_graph_n50
+# ./init_experiment.sh complete_graph_n50
 
+ansible-playbook -i inventory.ini playbook.yml -e "graph=graph_dblp_coauthorship_n317080"
+# ansible-playbook -i inventory.ini verify_playbook.yml -e "graph=graph_dblp_coauthorship_n317080" -e "date=" -e "job_id="
